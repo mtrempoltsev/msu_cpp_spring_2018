43b974053642a546cda08e4b4c9c9d6fbaf5d6d2
Alexander Chernyshev <alexander.v.chernyshev@mail.ru>
2018-02-22 22:15:18 +0300

    Some fixes everywhere

--- homework/Chernyshev/01/readme.md
+++ homework/Chernyshev/01/readme.md
@@ -1,10 +1,10 @@
 # Homework 01
-Computing sum of 2D-matrix elements
+Computing sum of 2D-matrix elements in two different ways: by rows and by columns
 
 ## Running tests
 This command will compile code and run programm on tests several times
 ```
-./run_tests.sh
+./run.sh
 ```
 
 ## Manual building and running
@@ -19,3 +19,12 @@ To run project manualy:
 ./sum_by_cols
 ```
 
+## Cleaning directory
+```
+make clean
+```
+
+## Results
+Running time for summation by rows: 0,25498s
+Running time for summation by columns: 0,75137s
+As we can see, the first algorithm is 3 times faster than the second one
--- homework/Chernyshev/01/sum_by_cols.cpp
+++ homework/Chernyshev/01/sum_by_cols.cpp
@@ -4,13 +4,19 @@
 #include <random>
 #include <ctime>
 
-// Constants for matrix sizes and value limits
-namespace Limits
+// Constants for matrix sizes
+namespace Sizes
 {
-    constexpr uint32_t max_rows = 10'000;
-    constexpr uint32_t max_cols = Limits::max_rows;
-    constexpr uint32_t max_value = 10'000;
-    constexpr uint32_t min_value = -Limits::max_value;
+    constexpr size_t max_rows = 10'000;
+    constexpr size_t max_cols = max_rows;
+}
+
+// Constants for random generator
+namespace Random
+{
+    constexpr unsigned int seed = 0xbadf00d;
+    constexpr int min_value = -10'000;
+    constexpr int max_value = 10'000;
 }
 
 // Timer class
@@ -22,32 +28,41 @@ public:
     {
     }
 
-    void print_time() {
-        const auto finish = std::chrono::high_resolution_clock::now();
-        std::cout << std::chrono::duration_cast<std::chrono::microseconds>(finish - start_).count() << std::endl;
-    }
+    friend std::ostream &operator << (std::ostream &out, const Timer &timer);
 
 private:
+    auto get_time() const
+    {
+        const auto finish = std::chrono::high_resolution_clock::now();
+        return std::chrono::duration_cast<std::chrono::microseconds>(finish - start_).count();
+    }
+
     const std::chrono::high_resolution_clock::time_point start_;
 };
+
+std::ostream &operator << (std::ostream &out, const Timer &timer)
+{
+    out << timer.get_time() << std::endl;
+    return out;
+}
 
 int main()
 {
     // Setting sizes of matrix
-    uint32_t rows = Limits::max_rows;
-    uint32_t cols = Limits::max_cols;
+    auto rows = Sizes::max_rows;
+    auto cols = Sizes::max_cols;
 
     // Memory allocating
-    int *data = new int[rows * cols];
-    int **matrix = new int *[rows];
+    auto *data = new int[rows * cols];
+    auto **matrix = new int *[rows];
 
     for (int r = 0; r < rows; ++r) {
         matrix[r] = data + r * cols;
     }
 
     // Matrix filling
-    std::mt19937 gen(time(0));
-    std::uniform_int_distribution<int> uid(Limits::min_value, Limits::max_value);
+    std::mt19937 gen(Random::seed);
+    std::uniform_int_distribution<int> uid(Random::min_value, Random::max_value);
     for (int r = 0; r < rows; ++r) {
         for (int c = 0; c < cols; ++c) {
             matrix[r][c] = uid(gen) + r + c;
@@ -62,7 +77,7 @@ int main()
             sum += matrix[r][c];
         }
     }
-    timer.print_time();
+    std::cout << timer << std::endl;
 
     // Free memory
     delete[] matrix;
