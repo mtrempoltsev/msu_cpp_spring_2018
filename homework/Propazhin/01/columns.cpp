8d7f8b201110185148c55a59d9782d558bc4f88e
apropazhin <artem.propazhin@mail.ru>
2018-02-24 17:37:12 +0300

    fix Timer, iter

--- homework/Propazhin/01/columns.cpp
+++ homework/Propazhin/01/columns.cpp
@@ -5,21 +5,22 @@ int main()
     size_t SIZE = 10000;
 
     int **data = new int*[SIZE];
-    for (int i = 0; i < SIZE; i++)
+    for (size_t i = 0; i < SIZE; i++)
 	data[i] = new int[SIZE];
 
-    for (int i = 0; i < SIZE; i++)
-	for (int j = 0; j < SIZE; j++)
+    for (size_t i = 0; i < SIZE; i++)
+	for (size_t j = 0; j < SIZE; j++)
 	    data[i][j] = 1;
 
     volatile int sum = 0;
-    
-    Timer t;
-    for (int i = 0; i < SIZE; i++)
-	for (int j = 0; j < SIZE; j++)
-	    sum += data[j][i];
 
-    for (int i = 0; i < SIZE; i++)
+  {  
+    Timer t;
+    for (size_t i = 0; i < SIZE; i++)
+	for (size_t j = 0; j < SIZE; j++)
+	    sum += data[j][i];
+  }
+    for (size_t i = 0; i < SIZE; i++)
 	delete data[i];
     delete[] data;
 
