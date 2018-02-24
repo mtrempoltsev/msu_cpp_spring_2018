af69b509a1ea61fad772f094589c2222f5692386
alexfmsu <alexfmsu@mail.ru>
2018-02-24 16:30:18 +0300

    alexfmsu: task_1 fixed

--- homework/Kulagin/01/sum_by_cols.cpp
+++ homework/Kulagin/01/sum_by_cols.cpp
@@ -9,14 +9,14 @@ using namespace std;
 int main() {
 	int** data = (int**)malloc(N * sizeof(int*));
 
-	for (int i = 0; i < N; i++) {
+	for (size_t i = 0; i < N; i++) {
 		data[i] = (int*)malloc(N * sizeof(int));
 	}
 
-	srand((unsigned)time(0));
+	srand(time(nullptr));
 
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
+	for (size_t i = 0; i < N; i++) {
+		for (size_t j = 0; j < N; j++) {
 			data[i][j] = rand();
 		}
 	}
@@ -28,8 +28,8 @@ int main() {
 
 		auto t1 = std::chrono::high_resolution_clock::now();
 
-		for (int j = 0; j < N; j++) {
-			for (int i = 0; i < N; i++) {
+		for (size_t j = 0; j < N; j++) {
+			for (size_t i = 0; i < N; i++) {
 				sum += data[i][j];
 			}
 		}
@@ -41,11 +41,11 @@ int main() {
 
 	cout << t / MAX_ITERATIONS << " us" << endl;
 
-	for (int i = 0; i < N; i++) {
-		delete[] data[i];
+	for (size_t i = 0; i < N; i++) {
+		free(data[i]);
 	}
 
-	delete[] data;
+	free(data);
 
 	return 0;
 }
