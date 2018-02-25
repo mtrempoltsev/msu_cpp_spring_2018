965fe9128b680d5640d3ea195ead724bd11ee4d0
Sergey <lalkakonus@gmail.com>
2018-02-25 23:18:20 +0300

    Upload *.cpp files

---
+++ homework/Kononov/1/sum2.cpp
@@ -0,0 +1,48 @@
+//Kononov Sergey group BD-11
+
+#include <chrono>
+#include <iostream>
+
+class Timer
+{
+	public:
+		Timer():start_(std::chrono::high_resolution_clock::now())
+		{}
+
+		~Timer()
+		{
+			const auto finish = std::chrono::high_resolution_clock::now();
+		        std::cout << std::chrono::duration_cast<std::chrono::microseconds>(finish - start_).count() << " us" << std::endl;
+		}
+
+	private:
+	const std::chrono::high_resolution_clock::time_point start_;
+};
+
+int main()
+{
+	Timer t;
+	const int DIM = 10000;
+
+	int** array = new int * [DIM];
+
+	int sum = 0;
+
+	for (int j = 0; j < DIM; ++j)
+		array[j] = new int [DIM];
+
+	for (int j = 0; j < DIM; ++j)
+		for (int i = 0; i < DIM; ++i)
+			array[i][j] = 0;
+	
+	for (int j = 0; j < DIM; ++j)
+		for (int i = 0; i < DIM; ++i)
+			sum += array[i][j];
+	
+	for (int i = 0; i < DIM; ++i)
+		delete [] array[i];
+
+	delete [] array;
+	
+	return 0;
+}
