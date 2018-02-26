cf21194572cb897e02b9349efae5acf6c50759a4
bolychevanton <36852755+bolychevanton@users.noreply.github.com>
2018-02-27 00:00:44 +0300

    Add files via upload
    Adding .cpp file of the first homework

---
+++ homework/Bolychev/01/cols_vs_rows.cpp
@@ -0,0 +1,72 @@
+#include <chrono>
+#include <iostream>
+
+class Timer
+{
+public:
+    Timer()
+    : start_(std::chrono::high_resolution_clock::now())
+    {
+    }
+    
+    ~Timer()
+    {
+        const auto finish = std::chrono::high_resolution_clock::now();
+        std::cout << std::chrono::duration_cast<std::chrono::microseconds>(finish - start_).count() << " us" << std::endl;
+    }
+    
+private:
+    const std::chrono::high_resolution_clock::time_point start_;
+};
+
+const size_t N = 1000;
+
+void initArray(int** arr)
+{
+    for (size_t i = 0; i < N; i++) {
+        for (size_t j = 0; j < N; j++) {
+            arr[i][j] = i^2 + j;
+        }
+    }
+}
+
+int main()
+{
+    int** arr = new int*[N];
+    
+    for (size_t i = 0; i < N; i++) {
+        arr[i] = new int[N];
+    }
+    
+    initArray(arr);
+    
+    
+    std::cout << "Time for sum by rows:" << std::endl;
+    int sum_by_rows = 0;
+    {
+        Timer t_sum_by_rows;
+        for (size_t i = 0; i < N; i++) {
+            for (size_t j = 0; j < N; j++) {
+                sum_by_rows += arr[i][j];
+            }
+        }
+    }
+    
+    std::cout << sum_by_rows << std::endl << std::endl <<
+        "Time for sum by columns:" << std::endl;
+    int sum_by_cols = 0;
+    {
+        Timer t_sum_by_cols;
+        for (size_t j = 0; j < N; j++) {
+            for (size_t i = 0; i < N; i++) {
+                sum_by_cols += arr[i][j];
+            }
+        }
+    }
+    std::cout << sum_by_cols << std::endl;
+    
+    for (size_t i = 0; i < N; i++) {
+        delete [] arr[i];
+    }
+    delete [] arr;
+}
