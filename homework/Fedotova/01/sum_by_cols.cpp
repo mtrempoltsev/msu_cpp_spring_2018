f5dde4f623b0a6ab967dd40ce35f1c29cf626a8b
NikitP <36661786+NikitP@users.noreply.github.com>
2018-03-01 20:37:39 +0300

    Update sum_by_cols.cpp

--- homework/Fedotova/01/sum_by_cols.cpp
+++ homework/Fedotova/01/sum_by_cols.cpp
@@ -1,53 +1,62 @@
 #include <chrono>
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-#define SIZE_r 10000
-#define SIZE_c 10000
+#include <vector>
 
 using namespace std;
 
-class Timer
-{
+const size_t size_of_rows = 10000;
+const size_t size_of_cols = 10000;
+
+class Timer {
 public:
     Timer()
-        : start_(std::chrono::high_resolution_clock::now())
-    {
+            : start_(chrono::high_resolution_clock::now()) {
     }
 
-    ~Timer()
-    {
-        const auto finish = std::chrono::high_resolution_clock::now();
-        std::cout << std::chrono::duration_cast<std::chrono::microseconds>(finish - start_).count() << " us" << std::endl;
+    ~Timer() {
+        const auto finish = chrono::high_resolution_clock::now();
+        cout << chrono::duration_cast<chrono::microseconds>(finish - start_).count() << " us.";
     }
 
 private:
-    const std::chrono::high_resolution_clock::time_point start_;
+    const chrono::high_resolution_clock::time_point start_;
 };
 
+void sum_by_col(int **a, size_t num_col, size_t num_row) {
+    Timer t;
+    volatile int sum = 0;
+    for (size_t i = 0; i < num_row; i++) {
+        for (size_t j = 0; j < num_col; j++) {
+            sum += a[j][i];
+        }
+    }
+}
+
+void sum_by_row(int **a, size_t num_col, size_t num_row) {
+    Timer t;
+    volatile int sum = 0;
+    for (size_t i = 0; i < num_row; i++) {
+        for (size_t j = 0; j < num_col; j++) {
+            sum += a[i][j];
+        }
+    }
+}
 
 int main() {
-	srand(time(NULL));
+    int **a = new int *[size_of_rows];
+    for (size_t i = 0; i < size_of_rows; i++) {
+        a[i] = new int[size_of_cols];
+    }
+    cout << "Time for cols: ";
+    sum_by_col(a, size_of_cols, size_of_rows);
+    cout << endl;
+    cout << "Time for rows: ";
+    sum_by_row(a, size_of_cols, size_of_rows);
 
-	int ** a = new int * [SIZE_r];
-	for (int i = 0; i < SIZE_r; i++) {
-		a[i] = new int [SIZE_c];
-		for (int j = 0; j < SIZE_c; j++)
-			a[i][j] = rand();
-	}
+    for (size_t i = 0; i < size_of_rows; i++) {
+        delete[] a[i];
+    }
+    delete[] a;
 
-	int sum = 0;
-	{
-		Timer t;
-		for (int i = 0; i < SIZE_c; i++)
-			for (int j = 0; j < SIZE_r; j++)
-				sum += a[j][i];
-	}
-	cout << sum << endl;
-
-	for (int i = 0; i < SIZE_r; i++)
-		delete[] a[i];
-	delete[] a;
-
-	return 0;
+    return 0;
 }
