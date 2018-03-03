2060e6974ab80259055e32b99e9ae6e7e126d8bd
Anna <a.akenteva@postgrespro.ru>
2018-03-04 00:59:35 +0300

    Fixed comments, removed tabs, removed global variables

--- homework/Akenteva/02/hw2.cpp
+++ homework/Akenteva/02/hw2.cpp
@@ -6,20 +6,20 @@ int binary_search(int const arr[], int arr_sz, int needed_num);
 int find_alike(int const arr[], int arr_sz, int pos, int dir);
 void init_eratosthenes_mask(bool mask[], int mask_sz);
 
-/* mask that will tell us which numbers are prime */
-const int max_num = Data[Size - 1];
-bool primes_mask[max_num];
-
 int
 main(int argc, char *argv[])
 {
     /* Note: Data[] and Size loaded from "numbers.dat" */
     int start_num,  s_pos;
     int finish_num, f_pos;
+
+    /* Create and init mask that will tell us which numbers are prime */
+    const int max_num = Data[Size - 1];
+    bool * primes_mask = new bool[max_num];
     init_eratosthenes_mask(primes_mask, max_num);
 
     /* we require an even number of arguments */
-    if((argc % 2) && (argc > 1))
+    if ((argc % 2) && (argc > 1))
     {
         /* looping through pairs of arguments */
         for (int i = 1; i < argc; i += 2)
@@ -27,19 +27,19 @@ main(int argc, char *argv[])
             start_num  = std::atoi(argv[i]);
             finish_num = std::atoi(argv[i + 1]);
             /* if impossible interval, print 0 and skip */
-            if(start_num > finish_num)
+            if (start_num > finish_num)
             {
                 printf("0\n");
                 continue;
             }
-            
+
             /* searching indexes of specified numbers */
             s_pos = binary_search(Data, Size, start_num);
             f_pos = binary_search(Data, Size, finish_num);
 
             /* if indexes don't meet this: 0 <= s_pos <= f_pos < Size,
                then print out 0 and don't do any further calculations */
-            if((f_pos >= Size) || (s_pos > f_pos) || (s_pos < 0))
+            if ((f_pos >= Size) || (s_pos > f_pos) || (s_pos < 0))
                 printf("0\n");
             else
             {
@@ -47,40 +47,44 @@ main(int argc, char *argv[])
                 s_pos = find_alike(Data, Size, s_pos, -1);
                 f_pos = find_alike(Data, Size, f_pos, 1);
                 int rez = 0;
-                for(int i = s_pos; i <= f_pos; i++)
+                for (int i = s_pos; i <= f_pos; i++)
                     rez += primes_mask[Data[i]];
                 printf("%d\n", rez);
             }
         }
+        delete [] primes_mask;
         return 0;
     }
     else
+    {
+        delete [] primes_mask;
         return -1;
+    }
 }
 
 /* binary_search()
  * Returns needed element's index in arr.
  * If doesn't find needed element, returns -1.
  * Takes - 1D sorted array(arr[]) + its size(arr_sz),
- *       - search borders(low, high),
- *       - number to search(needed).
+ *       - number to search(needed_num).
  */
 int
 binary_search(int const arr[], int arr_sz, int needed_num)
 {
     int mid, low = 0, high = arr_sz - 1;
     int curr_num;
-    while(1)
+    while (1)
     {
         mid = (low + high) / 2;
         curr_num = arr[mid];
-        if(needed_num > curr_num)
+        if (needed_num > curr_num)
             low  = mid + 1;
-        else if(needed_num < curr_num)
+        else if (needed_num < curr_num)
             high = mid - 1;
-        else return mid;
+        else
+            return mid;
 
-        if(low > high) return -1;
+        if (low > high) return -1;
     }
 }
 
@@ -92,16 +96,17 @@ binary_search(int const arr[], int arr_sz, int needed_num)
 int
 find_alike(int const arr[], int arr_sz, int pos, int dir)
 {
-    if((pos >= arr_sz) || (pos < 0))
+    if ((pos >= arr_sz) || (pos < 0))
         return -1;
 
     int needed_num = arr[pos];
-    do pos += dir;
+    do
+        pos += dir;
     while ((pos < arr_sz) && (pos >= 0) && (arr[pos] == needed_num));
     return (pos - dir);
 }
 
-/* eratosthenes_mask()
+/* init_eratosthenes_mask()
  * mask[i] = true  if i is a prime
  * mask[i] = false if i is NOT a prime
  * mask_sz should be >= 2
@@ -110,11 +115,10 @@ find_alike(int const arr[], int arr_sz, int pos, int dir)
  * For my original solution see the following commit:
  * 377121ee00611f781ddf3725a08b9f303a93d2f4
  */
-void
-init_eratosthenes_mask(bool mask[], int mask_sz)
+void init_eratosthenes_mask(bool mask[], int mask_sz)
 {
-	for(int i = 0; i < mask_sz; i++)
-		mask[i] = true;
+    for (int i = 0; i < mask_sz; i++)
+        mask[i] = true;
     mask[0] = mask[1] = false;
     for (int i = 2; i <= mask_sz; i++)
         if (mask[i] && (i * 1ll * i <= mask_sz))
