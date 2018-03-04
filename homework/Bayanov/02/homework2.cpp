54fa41325bb37f2fabf9c1ea56b422b23d7bd7e1
vitek <viktor.bayanov1993@mial.ru>
2018-03-04 16:47:32 +0300

    Fix second homework

--- homework/Bayanov/02/homework2.cpp
+++ homework/Bayanov/02/homework2.cpp
@@ -5,56 +5,58 @@
 
 const uint32_t MAX_VAL = 100001;
 
-std::vector<uint32_t> primes;
-uint32_t min_prime_div[MAX_VAL];
-
-void SieveEratosthenes() {
-    for (uint32_t it = 2; it < MAX_VAL; ++it) {
-        min_prime_div[it] = it;
+void SieveEratosthenes(std::vector<uint32_t>& min_prime_div,
+                       std::vector<uint32_t>& primes) {
+  for (uint32_t it = 2; it < MAX_VAL; ++it) {
+    min_prime_div[it] = it;
+  }
+  min_prime_div[0] = 1;
+  min_prime_div[1] = 0;
+  for (uint32_t it = 2; it < MAX_VAL; ++it) {
+    if (min_prime_div[it] == it) {
+      primes.push_back(it);
     }
-    min_prime_div[0] = 1;
-    min_prime_div[1] = 0;
-    for (uint32_t it = 2; it < MAX_VAL; ++it) {
-        if (min_prime_div[it] == it) {
-            primes.push_back(it);
-        }
-        for (uint32_t prime : primes) {
-            if (prime * it < MAX_VAL and prime <= min_prime_div[it]) {
-                min_prime_div[prime * it] = prime;
-            } else {
-                break;
-            }
-        }
+    for (uint32_t prime : primes) {
+      if (prime * it < MAX_VAL and prime <= min_prime_div[it]) {
+        min_prime_div[prime * it] = prime;
+      } else {
+        break;
+      }
     }
+  }
 }
 
 int main(int argc, char* argv[]) {
-    if (!(argc & 1) || argc == 1) return -1;
+  if (!(argc & 1) || argc == 1) return -1;
 
-    SieveEratosthenes();
+  std::vector<uint32_t> min_prime_div(MAX_VAL);
+  std::vector<uint32_t> primes;
 
-    for (uint32_t it = 1; it < argc; it += 2) {
-        uint32_t left_bound = atoi(argv[it]);
-        uint32_t right_bound = atoi(argv[it + 1]);
+  SieveEratosthenes(min_prime_div, primes);
 
-        bool left_is_here = false;
-        bool right_is_here = false;
+  for (uint32_t it = 1; it < argc; it += 2) {
+    uint32_t left_bound = atoi(argv[it]);
+    uint32_t right_bound = atoi(argv[it + 1]);
 
-        uint32_t primes_counter = 0;
+    bool left_is_here = false;
+    bool right_is_here = false;
 
-        for (size_t ind = 0; ind < Size; ++ind) {
-            if (left_bound <= Data[ind] and Data[ind] <= right_bound) {
-                primes_counter += (min_prime_div[Data[ind]] == Data[ind]);
-            }
-            left_is_here |= left_bound == Data[ind];
-            right_is_here |= right_bound == Data[ind];
-        }
+    uint32_t primes_counter = 0;
 
-        if (left_is_here && right_is_here) {
-            std::cout << primes_counter << "\n";
-        } else {
-            std::cout << 0 << "\n";
-        }
+    for (size_t ind = 0; ind < Size; ++ind) {
+      if (left_bound <= Data[ind] and Data[ind] <= right_bound) {
+        primes_counter += (min_prime_div[Data[ind]] == Data[ind]);
+      }
+      left_is_here |= left_bound == Data[ind];
+      right_is_here |= right_bound == Data[ind];
     }
-    return 0;
+
+    if (left_is_here && right_is_here) {
+      std::cout << primes_counter << "\n";
+    } else {
+      std::cout << 0 << "\n";
+    }
+  }
+
+  return 0;
 }
