f2dfbce6c2d487adb2a12a6da42d329f593aeb5e
vsheg <36817377+vsheg@users.noreply.github.com>
2018-03-05 08:39:31 +0300

    Add files via upload

---
+++ homework/Shegolev/bench_param.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// it's benchmark parameters
+const int fibonacci_number = 20;      // for heating I calculate the Fibonacci numbers
+const int max_size = 1024;            // array size takes values from 16 to max_size
+const int number_of_attempts = 10;    // number of identical measurements
+const int number_of_steps = 10;       // number of measurements with different dimensions
+
+/*
+ // on my computer
+ const int fibonacci_number = 45;
+ const int max_size = 16384; // 2^14
+ const int number_of_attempts = 10;
+ const int number_of_steps = 100;
+ */
