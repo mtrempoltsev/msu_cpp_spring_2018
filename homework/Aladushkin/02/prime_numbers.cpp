052374f6574468c9175e217687d1d850ba79556c
Aladushkin <danilaladushkin@gmail.com>
2018-03-05 14:29:15 +0300

    Fixed memory leak, unnecessary comments were deleted, ceil is cast to int, vector of pairs are not used, unchangeable values countOfPairs and sizeOfSieve now are const

--- homework/Aladushkin/02/prime_numbers.cpp
+++ homework/Aladushkin/02/prime_numbers.cpp
@@ -9,20 +9,17 @@
 
 #include "numbers.dat"
 
-// Data - массив
-// Size - размер массива
-
-bool* getSieve()
+bool* getSieve(const int sizeSieve)
 {
-	bool *result = new bool[100001];
-	std::fill_n(result, 100001, true);
+	bool *result = new bool[sizeSieve];
+	std::fill_n(result, sizeSieve, true);
 
 	result[0] = result[1] = false;
-	for (int i = 2; i < ceil(sqrt(100001)); i++)
+	for (int i = 2; i < int(ceil(sqrt(sizeSieve))); i++)
 	{
 		if (result[i])
 		{
-			for (int j = i*i; j < 100001; j += i)
+			for (int j = i*i; j < sizeSieve; j += i)
 				result[j] = false;
 		}
 	}
@@ -32,34 +29,25 @@ bool* getSieve()
 
 int main(int argc, char *argv[])
 {
-	auto sieve = getSieve();
-
-	//int Size;
-	//scanf("%i", &Size);
-
-	//std::unique_ptr<int[]> Data(new int[Size]);
-
-	//for (int i = 0; i < Size; i++)
-	//	scanf("%i", &Data[i]);
+	auto sieve = getSieve(100001);
 
 	if (argc % 2 == 0 || argc == 1)
 		return -1;
 
-	int countOfPairs = (argc-1) / 2;
-	std::vector<std::pair<int, int>> vec_min_max_pairs(countOfPairs);
+	const int countOfPairs = (argc-1) / 2;
+
+	int *max_arr = new int[countOfPairs];
+	int *min_arr = new int[countOfPairs];
 
 	for (int i = 0; i < countOfPairs; i++)
-	{ 
-
-		//int v_max = std::max(atoi(argv[1 + 2 * i]), atoi(argv[2 * i + 1 + 1]));
-		//int v_min = std::min(atoi(argv[1 + 2 * i]), atoi(argv[2 * i + 1 + 1]));
-
-		vec_min_max_pairs[i] = { atoi(argv[1 + 2 * i]), atoi(argv[2 * i + 1 + 1]) };
+	{
+		min_arr[i] = atoi(argv[1 + 2 * i]);
+		max_arr[i] = atoi(argv[2 * i + 1 + 1]);
 	}
 
 	for (int j = 0; j < countOfPairs; j++)
 	{
-		if (vec_min_max_pairs[j].first > vec_min_max_pairs[j].second)
+		if (min_arr[j] > max_arr[j])
 		{
 			std::cout << 0;
 			return 0;
@@ -76,7 +64,7 @@ int main(int argc, char *argv[])
 		int i = 0;
 		for (; i < Size; i++)
 		{
-			if (Data[i] == vec_min_max_pairs[j].first)
+			if (Data[i] == min_arr[j])
 			{
 				isMinExist = true;
 				indMin = i;
@@ -87,7 +75,7 @@ int main(int argc, char *argv[])
 		i = Size - 1;
 		for (; i >= 0; i--)
 		{
-			if (Data[i] == vec_min_max_pairs[j].second)
+			if (Data[i] == max_arr[j])
 			{
 				isMaxExist = true;
 				indMax = i;
@@ -110,6 +98,9 @@ int main(int argc, char *argv[])
 		std::cout << prime_counter << '\n';
 	}
 
-	return 0;
+	free(sieve);
+	free(min_arr);
+	free(max_arr);
 
+	return 0;
 }
