1c12e92f033a17a495d0b1217194383733f18588
jorki <jorki02@gmail.com>
2018-03-05 17:50:17 +0300

    Trunin's Second homework update

--- homework/Trunin/02/main.cpp
+++ homework/Trunin/02/main.cpp
@@ -3,52 +3,47 @@
 #include "numbers.dat"
 
 bool checkPrime(int number){
-	if(number == 1) return false;
-	for(int i = 2; i*i <= number; i++){
-		if(number % i == 0){
-			return false;
-		}
-	}
-	return true;
+    if(number == 1) return false;
+    for(int i = 2; i*i <= number; i++){
+        if(number % i == 0){
+            return false;
+        }
+    }
+    return true;
 }
 
 int findStartIndex(int start){
-	int min = 0;
-	int max = Size;
-	int middle;
-	while(min < max){
-		middle = (min + max) >> 1;
-		if(start > Data[middle])
-			min = middle + 1;
-		else 
-			max = middle; 
-	}
-	return min;
+    int min = 0;
+    int max = Size;
+    int middle;
+    while(min < max){
+        middle = (min + max) >> 1;
+        if(start > Data[middle])
+            min = middle + 1;
+        else 
+            max = middle; 
+    }
+    return min;
 }
 
 int findPrimes(int start_index, int end){
-	int sum = 0;
-	for(int i = start_index; Data[i] <= end; i++){
-		if(checkPrime(Data[i])){
-			sum++;
-		}
-	}
-	return sum;
+    int sum = 0;
+    for(int i = start_index; Data[i] <= end && i < Size ; i++){
+        if(checkPrime(Data[i])){
+            sum++;
+        }
+    }
+    return sum;
 }
 
 
 int main(int argc, char* argv[]){
-	if (!(argc & 1) || argc == 1) return -1;
-	
-	int pairs[argc - 1];  
-	for (int i = 0; i < argc - 1; i++)
+    if (!(argc & 1) || argc == 1) return -1;
+      
+    for (int i = 0; i < argc - 1; i +=2)
     {
-        pairs[i] = std::atoi(argv[i + 1]);
+        std::cout << findPrimes(findStartIndex(std::atoi(argv[i + 1])), std::atoi(argv[i + 2])) << std::endl;
     }
-   	for(int i = 0; i < argc - 1; i += 2){ 
-   		
-   		std::cout << findPrimes(findStartIndex(pairs[i]), pairs[i+1]) << std::endl;
-	}
     
     return 0;
 }
