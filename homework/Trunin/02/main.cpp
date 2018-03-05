a6a693888ab1f31231540d7d7842575f02dcf526
jorki <jorki02@gmail.com>
2018-03-05 21:09:17 +0300

    Trunin's second homework update 2

--- homework/Trunin/02/main.cpp
+++ homework/Trunin/02/main.cpp
@@ -12,13 +12,13 @@ bool checkPrime(int number){
     return true;
 }
 
-int findStartIndex(int start){
+int binarySearch(int value){
     int min = 0;
     int max = Size;
     int middle;
     while(min < max){
         middle = (min + max) >> 1;
-        if(start > Data[middle])
+        if(value > Data[middle])
             min = middle + 1;
         else 
             max = middle; 
@@ -38,11 +38,30 @@ int findPrimes(int start_index, int end){
 
 
 int main(int argc, char* argv[]){
+    
     if (!(argc & 1) || argc == 1) return -1;
-      
+    
+    int first_index = 0;
+    int first_value = 0; 
+    int second_value = 0;
+    int second_index = 0; 
+    
     for (int i = 0; i < argc - 1; i +=2)
     {
-        std::cout << findPrimes(findStartIndex(std::atoi(argv[i + 1])), std::atoi(argv[i + 2])) << std::endl;
+        first_value = std::atoi(argv[i + 1]);
+        first_index = binarySearch(first_value);
+        if(Data[first_index] != first_value){
+            std::cout << 0 << std::endl;
+            return 0;
+        }
+        
+        second_value = std::atoi(argv[i + 2]);
+        second_index = binarySearch(second_value);
+        if(Data[second_index] != second_value){
+            std::cout << 0 << std::endl;
+            return 0;
+        }
+        std::cout << findPrimes(first_index, second_value) << std::endl;
     }
     
     return 0;
