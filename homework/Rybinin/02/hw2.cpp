7f300f4952bb09bc9ca43b483193f776c7979c87
ArrowRa <32740002+ArrowRa@users.noreply.github.com>
2018-03-07 17:16:57 +0300

    Update hw2.cpp

--- homework/Rybinin/02/hw2.cpp
+++ homework/Rybinin/02/hw2.cpp
@@ -1,20 +1,37 @@
 #include <iostream>
 #include "numbers.dat"
 
+const int MAX_V = 1e5;
+
+int* find_primes(int asize)
+{
+    int *arr = (int *)malloc(asize * sizeof(int));
+    for(long long i = 3; i * i <= asize; i += 2)
+        for(long long j = i * i; j <= asize; j += 2 * i)
+            arr[j] = 1;
+    for(int i = 4; i <= asize; i += 2)
+        arr[i] = 1;
+    arr[0] = 1;
+    arr[1] = 1;
+    return arr;
+}
+
+int* create_map(int asize)
+{
+    int *arr = (int *)malloc(asize * sizeof(int));
+    for (int i = 0; i < asize; ++i)
+        arr[i] = 0;
+    for(int i = 0; i < Size; ++i)
+        if(!arr[Data[i]])
+            arr[Data[i]] = i + 1;
+    return arr;
+}
+
 int main(int argc, char* argv[])
 {
     if (argc % 2 == 0 || argc == 1) return -1;
-    int index[100010] = {0,};
-    int prost[100010] = {0,};
-    for(int i = 0; i < Size; ++i)
-        if(!index[Data[i]])
-            index[Data[i]] = i + 1;
-    for(long long i = 3; i * i <= 100000; i += 2)
-        for(long long j = i * i; j <= 100000; j += 2 * i)
-            prost[j] = 1;
-    for(int i = 4; i <= 100000; i += 2)
-        prost[i] = 1;
-    prost[1] = 1;
+    int *index = create_map(MAX_V + 1);
+    int *prime = find_primes(MAX_V + 1);
     for (int i = 1; i < argc; i += 2)
     {
         int start = std::atoi(argv[i]);
@@ -25,9 +42,11 @@ int main(int argc, char* argv[])
         }
         int cnt = 0;
         for(int j = index[start] - 1; j < index[endd]; ++j)
-            if(!prost[Data[j]])
+            if(!prime[Data[j]])
                 ++cnt;
         std::cout << cnt << std::endl;
     }
+    free(index);
+    free(prime);
     return 0;
 }
