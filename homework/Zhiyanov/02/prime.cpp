9730ad747efa5d5f4034479d1e756f70acb40985
ZhiyanovAP <antonzhiyanov17@gmail.com>
2018-03-12 19:05:32 +0300

    hw2

--- homework/Zhiyanov/02/prime.cpp
+++ homework/Zhiyanov/02/prime.cpp
@@ -3,23 +3,33 @@
 
 #include "numbers.dat"
 
-const int  N = 100000;
 
-int indata(int n){
+int indl(int n){
     for (int i = 0; i < Size; ++i)
-        if (n == Data[i])
+        if (n <= Data[i])
             return i;
+
     return -1;
+}
+
+int indr(int n){
+    if (n < 0)
+        return -1;
+    for (int i = 0; i < Size; ++i)
+        if (n < Data[i])
+            return i - 1;
+
+    return Size - 1;
 }
 
 bool isprime(int n){
     if (n <= 1)
-        return 0;
+        return False;
     
     for (int i = 2; i * i <= n; ++i)
         if (n % i == 0)
-            return 0;
-    return 1;
+            return False;
+    return True;
 }
 
 int main(int argc, char* argv[]){
@@ -28,19 +38,20 @@ int main(int argc, char* argv[]){
         return -1;
 
     
-    argc--;
+    int argcc = argc - 1;
 
-    int* ind = new int[argc];
-    for (int i = 0; i < argc; ++i){
-        ind[i] = indata(atoi(argv[i + 1]));
-        if (ind[i] == -1){
+    int* ind = new int[argcc];
+    for (int i = 0; i < argcc; i += 2){
+        ind[i] = indl(atoi(argv[i + 1]));
+        ind[i + 1] = indr(atoi(argv[i + 2]));
+        if (ind[i] == -1 || ind[i + 1] == -1){
             delete [] ind;
             return -1;
         }
     }
 
 
-    for (int i = 0; i < argc; i += 2){
+    for (int i = 0; i < argcc; i += 2){
         int count = 0;
         for (int j = ind[i]; j <= ind[i + 1]; ++j)
             if (isprime(Data[j]))
