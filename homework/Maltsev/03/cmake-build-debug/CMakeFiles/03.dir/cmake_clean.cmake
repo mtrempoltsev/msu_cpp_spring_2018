257583a96ad42a8ae936a4346d86ab3b9fb1cd78
MaltcevAN <you@example.com>
2018-03-15 02:38:25 +0300

    HW 3

---
+++ homework/Maltsev/03/cmake-build-debug/CMakeFiles/03.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/03.dir/main.cpp.o"
+  "03.pdb"
+  "03"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/03.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
