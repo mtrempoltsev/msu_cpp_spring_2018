2fa9e975d4e949085b80e1c6bf7b757649d1e05f
ArrowRa <32740002+ArrowRa@users.noreply.github.com>
2018-03-15 15:53:31 +0300

    Create hw03.h

---
+++ homework/Rybinin/03/hw03.h
@@ -0,0 +1,82 @@
+
+class Inanimate
+{
+    const std::string name;
+    const int max_durability;
+    int durability;
+};
+
+class Weapon: public Inanimate
+{
+    const int damage;
+    const int speed;
+};
+
+class Shovel: public Weapon {
+
+};
+
+class Sword: public Weapon {
+
+};
+
+class Bow: public Weapon {
+
+};
+
+class Armor: public Inanimate
+{
+    const int defence;
+};
+
+class Lats: public Armor {
+
+};
+
+class ChainArmor: public Armor {
+
+};
+
+class Animate
+{
+    const std::string name;
+    const int max_health;
+    int health;
+};
+
+class Humans: public Animate
+{
+    const int damage;
+    const int defence;
+};
+
+class Animal: public Animate
+{
+    Humans *owner;
+};
+
+class Pig: public Animal {
+
+};
+
+class Knight: public Humans
+{
+    Sword *weapon;
+    Lats *armor;
+
+};
+
+class Archer: public Humans
+{
+    Bow *weapon;
+    ChainArmor *armor;
+};
+
+class Peasant: public Humans
+{
+    Shovel *weapon;
+    Pig *animal;
+};
+
+
+
