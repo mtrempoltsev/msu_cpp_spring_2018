b00f3d24f2425007c9ec73e2e2790a494ec68263
antosiv <anton6722@gmail.com>
2018-03-16 21:38:01 +0300

    first dispatch

---
+++ homework/Sivkov/04/main.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+
+class Matrix {
+public:
+    class Vector {
+    public:
+        explicit Vector(size_t size) : size(size) {
+            data = new int(size);
+            for (size_t i = 0; i < size; ++i)
+                data[i] = 1;
+        }
+
+        size_t GetSize() const {
+            return size;
+        }
+
+        const int &operator[](size_t index) const {
+            if (index < 0 || index >= size)
+                throw std::out_of_range("");
+            return data[index];
+        }
+
+        int &operator[](size_t index) {
+            if (index < 0 || index >= size)
+                throw std::out_of_range("");
+            return data[index];
+        }
+
+        bool operator==(const Vector &other) const {
+            if (other.GetSize() != size)
+                return false;
+            bool flag = true;
+            for (size_t i = 0; i < size; ++i)
+                if (data[i] != other[i])
+                    flag = false;
+            return flag;
+        }
+
+        bool operator!=(const Vector &other) const {
+            return !((*this) == other);
+        }
+
+        Vector &operator*=(int factor) {
+            for (size_t i = 0; i < size; ++i)
+                data[i] *= factor;
+            return *this;
+        }
+
+    private:
+        int *data;
+        size_t size;
+    };
+
+    Matrix(size_t cols, size_t rows) : cols(cols), rows(rows) {
+        data = static_cast<Vector *>(operator new[](cols * sizeof(Vector)));
+        for (size_t i = 0; i < cols; ++i) {
+            new(data + i) Vector(rows);
+        }
+    }
+
+    const Vector &operator[](size_t index) const {
+        if (index < 0 || index >= cols)
+            throw std::out_of_range("");
+        return data[index];
+    }
+
+    Vector &operator[](size_t index) {
+        if (index < 0 || index >= cols)
+            throw std::out_of_range("");
+        return data[index];
+    }
+
+    bool operator==(const Matrix &other) const {
+        if (other.getColumns() != cols || other.getRows() != rows)
+            return false;
+        bool flag = true;
+        for (size_t i = 0; i < cols; ++i)
+            if (data[i] != other[i])
+                flag = false;
+        return flag;
+    }
+
+    bool operator!=(const Matrix &other) const {
+        return !((*this) == other);
+    }
+
+    Matrix &operator*=(int factor) {
+        for (size_t i = 0; i < cols; ++i)
+            data[i] *= factor;
+        return *this;
+    }
+
+    size_t getRows() const {
+        return rows;
+    }
+
+    size_t getColumns() const {
+        return cols;
+    }
+
+private:
+    Vector *data;
+    size_t cols;
+    size_t rows;
+};
+
+
+int main() {
+    Matrix a(2, 2);
+    //Matrix b(2, 2);
+    a[0][0] = 2;
+    a *= 2;
+    int b = a[0][0];
+    std::cout << a[0][0];
+    return 0;
+}
