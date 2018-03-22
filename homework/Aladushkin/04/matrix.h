ba45d9d106d6ef9e997a7848553a0afe20b657f7
Aladushkin <danilaladushkin@gmail.com>
2018-03-22 13:12:15 +0300

    Fixed type errors(int and size_t), fixed operator!=, fixed conditions (>= -> row - 1)

--- homework/Aladushkin/04/matrix.h
+++ homework/Aladushkin/04/matrix.h
@@ -6,112 +6,109 @@
 class Array
 {
 public:
-	const int& operator[](int row_) const
-	{
-		int row = array.size();
-		if (row_ >= row)
-			throw std::out_of_range("");
+        const int& operator[](int row_) const
+        {
+                size_t row = array.size();
+                if (row_ > row - 1)
+                        throw std::out_of_range("");
 
-		return array[row_];
-	}
+                return array[row_];
+        }
 
-	int& operator[](int row_)
-	{
-		int row = array.size();
-		if (row_ >= row)
-			throw std::out_of_range("");
+        int& operator[](int row_)
+        {
+                size_t row = array.size();
+                if (row_ > row - 1)
+                        throw std::out_of_range("");
 
-		return array[row_];
-	}
+                return array[row_];
+        }
 
-	Array& operator*=(int val)
-	{
-		std::for_each(array.begin(), array.end(), [val](int &n) { n *= val; });
-		return *this;
-	}
+        Array& operator*=(int val)
+        {
+                std::for_each(array.begin(), array.end(), [val](int &n) { n *= val; });
+                return *this;
+        }
 
-	bool operator==(const Array& other) const
-	{
-		return array == other.array;
-	}
+        bool operator==(const Array& other) const
+        {
+                return array == other.array;
+        }
 
-	bool operator!=(const Array& other) const
-	{
-		array != other.array;
-	}
+        bool operator!=(const Array& other) const
+        {
+                array != other.array;
+        }
 
-	void resize(size_t new_size)
-	{
-		array.resize(new_size);
-	}
+        void resize(size_t new_size)
+        {
+                array.resize(new_size);
+        }
 
 private:
-	std::vector<int> array;
+        std::vector<int> array;
 };
 
 class Matrix
 {
 private:
-	std::vector<Array> matrix;
+        std::vector<Array> matrix;
 
-	int row;
-	int col;
+        int row;
+        int col;
 
 public:
 
-	int getRows() { return row; }
-	int getColumns() { return col; }
+        int getRows() { return row; }
+        int getColumns() { return col; }
 
-	Matrix(const int col_, const int row_)
-	{
-		row = row_;
-		col = col_;
+        Matrix(const int row_, const int col_)
+        {
+                row = row_;
+                col = col_;
 
-		matrix.resize(col);
+                matrix.resize(row);
 
-		for (int i = 0; i < col; i++)
-			matrix[i].resize(row);
-	}
+                for (int i = 0; i < row; i++)
+                        matrix[i].resize(col);
+        }
 
-	const Array& operator[](int row_) const
-	{
-		if (row_ > row || row == 0)
-			throw std::out_of_range("");
-		return matrix[row_];
-	}
+        const Array& operator[](int row_) const
+        {
+                if (row_ > row - 1)
+                        throw std::out_of_range("");
+                return matrix[row_];
+        }
 
-	Array& operator[](int row_)
-	{
-		if (row_ > row || row == 0)
-			throw std::out_of_range("");
-		return matrix[row_];
-	}
+        Array& operator[](int row_)
+        {
+                if (row_ > row - 1)
+                        throw std::out_of_range("");
+                return matrix[row_];
+        }
 
-	Matrix& operator*=(int val)
-	{
-		std::for_each(matrix.begin(), matrix.end(), [val](Array &vec) { vec *= val; });
-		return *this;
-	}
+        Matrix& operator*=(int val)
+        {
+                std::for_each(matrix.begin(), matrix.end(), [val](Array &vec) { vec *= val; });
+                return *this;
+        }
 
-	bool operator==(const Matrix& other) const
-	{
-		if (!(row == other.row && col == other.col))
-			return false;
+        bool operator==(const Matrix& other) const
+        {
+                if (!(row == other.row && col == other.col))
+                        return false;
 
-		for (int i = 0; i < row; i++)
-		{
-			if (!(matrix[i] == other[i]))
-				return false;
-		}
+                for (int i = 0; i < row; i++)
+                {
+                        if (!(matrix[i] == other[i]))
+                                return false;
+                }
 
-		return true;
-	}
+                return true;
+        }
 
-	bool operator!=(const Matrix& other) const
-	{
-		if (!(row == other.row && col == other.col))
-			return true;
-
-		return !(*this == other);
-	}
+        bool operator!=(const Matrix& other) const
+        {
+                return !(*this == other);
+        }
 };
