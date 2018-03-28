12771451d6876370244c49108b34bf856b6a4293
Ivan Ilyin <vanyail@yandex.ru>
2018-03-28 12:52:40 +0300

    Add files via upload

--- homework/Ilyin/04/matrix.h
+++ homework/Ilyin/04/matrix.h
@@ -4,103 +4,100 @@
 #include <memory>
 
 struct Row_proxy{
-size_t rows;
-std::unique_ptr<int []> storage;
+    size_t rows;
+    std::unique_ptr<int []> storage;
 
-Row_proxy(size_t n){
-    rows = n;
-    storage.reset(new int [rows]);
-}
-
-int &operator [](size_t i){
-    if (i >= rows){
-        throw std::out_of_range("Out of range, rows");
+    Row_proxy(size_t n){
+        rows = n;
+        storage.reset(new int [rows]);
     }
-    return storage[i];
-}
 
-const int &operator [](size_t i) const{
-    if (i >= rows){
-        throw std::out_of_range("Out of range, rows");
+    int &operator [](size_t i){
+        if (i >= rows){
+            throw std::out_of_range("Out of range, rows");
+        }
+        return storage[i];
     }
-    return storage[i];
-}
+
+    const int &operator [](size_t i) const{
+        if (i >= rows){
+            throw std::out_of_range("Out of range, rows");
+        }
+        return storage[i];
+    }
 };
 
 class Matrix{
 private:
-size_t rows;
-size_t cols;
-std::unique_ptr<Row_proxy []> data;
+    size_t rows;
+    size_t cols;
+    std::unique_ptr<Row_proxy []> data;
 
 public:
-Row_proxy & operator [](size_t i){
-    if (i >= cols){
-        throw std::out_of_range("Out of range, cols");
+    Row_proxy & operator [](size_t i){
+        if (i >= cols){
+            throw std::out_of_range("Out of range, cols");
+        }
+        return data[i];
     }
-    return data[i];
-}
 
-const Row_proxy & operator [](size_t i) const{
-    if (i >= cols){
-        throw std::out_of_range("Out of range, cols");
+    const Row_proxy & operator [](size_t i) const{
+        if (i >= cols){
+            throw std::out_of_range("Out of range, cols");
+        }
+        return data[i];
     }
-    return data[i];
-}
 
-size_t getColumns() const{
-    return cols;
-}
-
-size_t getRows() const{
-    return rows;
-}
-
-Matrix(const size_t n, const size_t m){
-    rows = m;
-    cols = n;
-    data.reset(static_cast<Row_proxy*>(operator new [] (cols * sizeof(Row_proxy))));
-
-    for (size_t i = 0; i < cols; i++){
-        new(data.get() + i) Row_proxy(rows);
+    size_t getColumns() const{
+        return cols;
     }
-}
 
-~Matrix(){
-    for (size_t i = 0; i < cols; i++){
-       data[i].~Row_proxy();
+    size_t getRows() const{
+        return rows;
     }
-    data.release();
-}
 
-Matrix & operator *=(int mult){
-    for (size_t i = 0; i < rows; i++){
-        for (size_t j = 0; j < cols; j++){
-            data[j][i] *= mult;
+    Matrix(const size_t n, const size_t m){
+        rows = m;
+        cols = n;
+        data.reset(static_cast<Row_proxy*>(operator new [] (cols * sizeof(Row_proxy))));
+        for (size_t i = 0; i < cols; i++){
+            new(data.get() + i) Row_proxy(rows);
         }
     }
-    return *this;
-}
 
-bool operator ==(const Matrix &a) const{
-    if (rows != a.getRows() || cols != a.getColumns()){
-        return false;
+    ~Matrix(){
+        for (size_t i = 0; i < cols; i++){
+           data[i].~Row_proxy();
+        }
+        data.release();
     }
-    
-    for (size_t i = 0; i < rows; i++){
-        for (size_t j = 0; j < cols; j++){
-            if ( !(data[j][i] == a[j][i]) ){
-                return false;
+
+    Matrix & operator *=(int mult){
+        for (size_t i = 0; i < rows; i++){
+            for (size_t j = 0; j < cols; j++){
+                data[j][i] *= mult;
             }
         }
+        return *this;
     }
-    return true;
-}
 
-bool operator !=(const Matrix &a) const{
-    return !(*this == a);
-}
+    bool operator ==(const Matrix &a) const{
+        if (rows != a.getRows() || cols != a.getColumns()){
+            return false;
+        }
+        for (size_t i = 0; i < rows; i++){
+            for (size_t j = 0; j < cols; j++){
+                if ( !(data[j][i] == a[j][i]) ){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
+    bool operator !=(const Matrix &a) const{
+        return !(*this == a);
+    }
 };
 
-#endif
+#endif //MATRIX_H
