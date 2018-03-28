ba78d519f2eb555900bf5837bc091e4d20d033f5
Алексей <aj.kuznecov@physics.msu.ru>
2018-03-29 02:54:32 +0300

    fix consts and signature

--- homework/Kuznetsov/04/matrix.h
+++ homework/Kuznetsov/04/matrix.h
@@ -8,7 +8,12 @@ public:
 		size_t len;
 		int* row;
 		Proxy(int row[], size_t len) : row(row), len(len) {};
-		int& operator[](size_t i) const{
+		const int& operator[](size_t i) const{
+			if (i >= len)
+				throw std::out_of_range("index out of range");
+			return row[i];
+		}
+		int& operator[](size_t i)  {
 			if (i >= len)
 				throw std::out_of_range("index out of range");
 			return row[i];
@@ -33,31 +38,38 @@ public:
 	size_t getColumns() const{
 		return columnCount;
 	}
-	Proxy operator[](size_t i) const{
+	Proxy operator[](size_t i){
 		if (i >= rowCount)
 			throw std::out_of_range("index out of range");
 		return Proxy(matrix[i], columnCount);
 	}
-	Matrix operator*=(int number) {
+	const Proxy operator[](size_t i) const{
+		if (i >= rowCount)
+			throw std::out_of_range("index out of range");
+		return Proxy(matrix[i], columnCount);
+	}
+	
+	void operator*=(int number) {
 		for (size_t i = 0; i < rowCount; i++) {
 			for (size_t j = 0; j < columnCount; j++) {
 				matrix[i][j] = matrix[i][j] * number;
 			}
 		}
-		return *this;
+		
 	}
-	bool operator==(Matrix other) const{
+	bool operator==(const Matrix& other) const{
 		if (other.rowCount != rowCount || other.columnCount != columnCount)
 			return 0;
+
 		for (size_t i = 0; i < rowCount; i++) {
 			for (size_t j = 0; j < columnCount; j++) {
-				if (matrix[i][j] != other[i][j])
+				if (matrix[i][j] != other.matrix[i][j])
 					return 0;
 			}
 		}
 		return 1;
 	}
-	bool operator!=(Matrix other) const{
+	bool operator!=(const Matrix& other) const{
 		return !(*this == other);
 	}
 
