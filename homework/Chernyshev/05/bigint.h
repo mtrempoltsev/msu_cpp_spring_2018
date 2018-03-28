4402dfec99108e46647e3e9df655343f9b16f502
Alexander Chernyshev <alexander.v.chernyshev@mail.ru>
2018-03-28 23:01:34 +0300

    tabs fixed

--- homework/Chernyshev/05/bigint.h
+++ homework/Chernyshev/05/bigint.h
@@ -216,8 +216,8 @@ class BigInt
     };
     constexpr static int BASE = 1'000'000'000;
 
-	bool sign = positive;
-	Vector<int> number;
+    bool sign = positive;
+    Vector<int> number;
 
 
     void delete_zeros()
@@ -232,31 +232,31 @@ class BigInt
     }
 
 public:
-	// Constructor from std::string
-	BigInt(const std::string &$number)
-	{
-		sign = positive;
+    // Constructor from std::string
+    BigInt(const std::string &$number)
+    {
+        sign = positive;
 
-		auto n = $number.size();
-		if (n == 0) {
+        auto n = $number.size();
+        if (n == 0) {
             return;
         }
 
-		int start = 0;
-		if ($number[0] == '-') {
-			sign = negative;
-			start = 1;
-		}
+        int start = 0;
+        if ($number[0] == '-') {
+            sign = negative;
+            start = 1;
+        }
 
-		for (int i = n; i > start; i -= 9)
-		{
-			if (i - start < 9) {
+        for (int i = n; i > start; i -= 9)
+        {
+            if (i - start < 9) {
                 number.push_back(stoi($number.substr(start, i)));
             } else {
                 number.push_back(stoi($number.substr(i - 9, 9)));
             }
-		}
-	}
+        }
+    }
 
     // Constructor from const char *
     BigInt(const char *$number):
@@ -264,20 +264,20 @@ public:
     {
     }
 
-	// Constructor from long long
-	BigInt(long long $number = 0)
-	{
-		sign = positive;
-		if ($number < 0) {
-			sign = negative;
-			$number = -$number;
-		}
+    // Constructor from long long
+    BigInt(long long $number = 0)
+    {
+        sign = positive;
+        if ($number < 0) {
+            sign = negative;
+            $number = -$number;
+        }
 
-		do {
-			number.push_back($number % BASE);
-			$number /= BASE;
-		} while ($number > 0);
-	}
+        do {
+            number.push_back($number % BASE);
+            $number /= BASE;
+        } while ($number > 0);
+    }
 
     // For correct convertion of '0' to BigInt (there's problem with const char* constructor)
     BigInt(int $number)
@@ -285,11 +285,11 @@ public:
     {
     }
 
-	// Copy constructors
-	BigInt(const BigInt &a)
+    // Copy constructors
+    BigInt(const BigInt &a)
             : sign(a.sign), number(a.number)
-	{
-	}
+    {
+    }
 
     BigInt(BigInt &&a)
             : sign(a.sign), number(a.number)
@@ -310,262 +310,262 @@ public:
         return number.size();
     }
 
-	bool operator < (const BigInt &a) const
-	{
-		const int n = size();
-		const int m = a.size();
+    bool operator < (const BigInt &a) const
+    {
+        const int n = size();
+        const int m = a.size();
 
-		if (n == 1 && m == 1 && number[0] == 0 && a.number[0] == 0) {
+        if (n == 1 && m == 1 && number[0] == 0 && a.number[0] == 0) {
             return false;
         }
 
-		if (sign == positive && a.sign == negative) {
+        if (sign == positive && a.sign == negative) {
             return false;
         }
-		if (sign == negative && a.sign == positive) {
+        if (sign == negative && a.sign == positive) {
             return true;
         }
 
-		bool is_less = true;
-		if (n < m) {
+        bool is_less = true;
+        if (n < m) {
             is_less = true;
         } else if (n == m) {
-			bool are_equal = true;
+            bool are_equal = true;
             for (int i = n - 1; i >= 0; --i) {
-				if (number[i] != a.number[i]) {
-					is_less = number[i] < a.number[i];
-					are_equal = false;
-					break;
-				}
-			}
+                if (number[i] != a.number[i]) {
+                    is_less = number[i] < a.number[i];
+                    are_equal = false;
+                    break;
+                }
+            }
 
-			if (are_equal) {     // I return "false" here because these two numbers
+            if (are_equal) {     // I return "false" here because these two numbers
                 return false;    // are equal so (*this < a) and (a < *this) at the same time
             }
-		} else {
+        } else {
             is_less = false;
         }
 
-		if (sign == negative) {
+        if (sign == negative) {
             is_less = !is_less;
         }
 
-		return is_less;
-	}
+        return is_less;
+    }
 
-	bool operator > (const BigInt &a) const
-	{
-		return a < *this;
-	}
+    bool operator > (const BigInt &a) const
+    {
+        return a < *this;
+    }
 
-	bool operator == (const BigInt &a) const
-	{
-		return !(*this < a || a < *this);
-	}
+    bool operator == (const BigInt &a) const
+    {
+        return !(*this < a || a < *this);
+    }
 
-	bool operator <= (const BigInt &a) const
-	{
+    bool operator <= (const BigInt &a) const
+    {
         return !(*this > a);
-	}
+    }
 
-	bool operator >= (const BigInt &a) const
-	{
+    bool operator >= (const BigInt &a) const
+    {
         return !(*this < a);
-	}
+    }
 
-	bool operator != (const BigInt &a) const
-	{
-		return !(*this == a);
-	}
+    bool operator != (const BigInt &a) const
+    {
+        return !(*this == a);
+    }
 
-	BigInt operator - () const
-	{
-		BigInt result = *this;
+    BigInt operator - () const
+    {
+        BigInt result = *this;
 
-		if (sign == positive) {
+        if (sign == positive) {
             result.sign = negative;
         } else {
             result.sign = positive;
         }
 
-		return result;
-	}
+        return result;
+    }
 
-	BigInt operator + (const BigInt &a) const
-	{
-		if (sign == a.sign) {
-			BigInt result;
+    BigInt operator + (const BigInt &a) const
+    {
+        if (sign == a.sign) {
+            BigInt result;
 
             result.number.resize(0);
-			result.sign = sign;
+            result.sign = sign;
 
-			int carry = 0;
-			int n = size();
-			int m = a.size();
+            int carry = 0;
+            int n = size();
+            int m = a.size();
 
-			for (int i = 0; i < std::max(n, m) || carry; ++i) {
-				int cur_digit = carry;
-				cur_digit += i < n ? number[i] : 0;
-				cur_digit += i < m ? a.number[i] : 0;
+            for (int i = 0; i < std::max(n, m) || carry; ++i) {
+                int cur_digit = carry;
+                cur_digit += i < n ? number[i] : 0;
+                cur_digit += i < m ? a.number[i] : 0;
 
-				carry = cur_digit >= BASE;
-				if(carry) {
+                carry = cur_digit >= BASE;
+                if(carry) {
                     cur_digit -= BASE;
                 }
 
-				result.number.push_back(cur_digit);
-			}
+                result.number.push_back(cur_digit);
+            }
 
-			return result;
-		} else {
-			if (sign == negative) {
+            return result;
+        } else {
+            if (sign == negative) {
                 return a - (-(*this));
             } else {
                 return *this - (-a);
             }
-		}
-	}
+        }
+    }
 
-	BigInt operator - (const BigInt &a) const
-	{
-		if (*this == a) {
+    BigInt operator - (const BigInt &a) const
+    {
+        if (*this == a) {
             return 0;
         }
 
-		if (sign == a.sign) {
-			if (a.sign == negative) {
+        if (sign == a.sign) {
+            if (a.sign == negative) {
                 return (-a) - (-(*this));
             }
 
-			BigInt result;
+            BigInt result;
             result.number.resize(0);
 
             BigInt fir = *this;
-			BigInt sec = a;
-			if (fir < sec) {
-				std::swap(fir, sec);
-				result.sign = negative;
-			}
+            BigInt sec = a;
+            if (fir < sec) {
+                std::swap(fir, sec);
+                result.sign = negative;
+            }
 
-			int carry = 0;
-			int n = fir.size();
-			int m = sec.size();
+            int carry = 0;
+            int n = fir.size();
+            int m = sec.size();
 
             for (int i = 0; i < n || i < m; ++i) {
-				int cur_digit = i < n ? fir.number[i] : 0;
-				cur_digit -= i < m ? sec.number[i] : 0;
-				cur_digit -= carry;
+                int cur_digit = i < n ? fir.number[i] : 0;
+                cur_digit -= i < m ? sec.number[i] : 0;
+                cur_digit -= carry;
 
-				carry = cur_digit < 0;
-				if (carry) {
+                carry = cur_digit < 0;
+                if (carry) {
                     cur_digit += BASE;
                 }
 
-				result.number.push_back(cur_digit);
-			}
+                result.number.push_back(cur_digit);
+            }
 
-			result.delete_zeros();
+            result.delete_zeros();
 
-			return result;
-		} else {
-			if (sign == negative) {
+            return result;
+        } else {
+            if (sign == negative) {
                 return -((-(*this)) + a);
             } else {
                 return *this + (-a);
             }
-		}
-	}
+        }
+    }
 
-	BigInt operator * (const BigInt &a) const
-	{
-		BigInt result;
-		result.number.resize(size() + a.size());
+    BigInt operator * (const BigInt &a) const
+    {
+        BigInt result;
+        result.number.resize(size() + a.size());
 
-		if (sign != a.sign) {
+        if (sign != a.sign) {
             result.sign = negative;
         }
 
-		int n = size();
-		int m = a.size();
+        int n = size();
+        int m = a.size();
 
-		for (int i = 0; i < n; ++i) {
-			for (int j = 0, carry = 0; j < m || carry; ++j) {
-				long long cur = result.number[i + j] + carry;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0, carry = 0; j < m || carry; ++j) {
+                long long cur = result.number[i + j] + carry;
                 if (j < m) {
                     cur += 1LL * number[i] * a.number[j];
                 }
-				result.number[i + j] = cur % BASE;
-				carry = cur / BASE;
-			}
-		}
+                result.number[i + j] = cur % BASE;
+                carry = cur / BASE;
+            }
+        }
 
-		result.delete_zeros();
+        result.delete_zeros();
 
-		return result;
-	}
+        return result;
+    }
 
-	BigInt operator / (const BigInt &a) const
-	{
-		BigInt result;
+    BigInt operator / (const BigInt &a) const
+    {
+        BigInt result;
         result.number.resize(0);
 
         if (sign != a.sign) {
             result.sign = negative;
         }
 
-		BigInt sec = a;
-		sec.sign = positive;
+        BigInt sec = a;
+        sec.sign = positive;
 
-		int n = size();
+        int n = size();
 
-		BigInt cur_value;
+        BigInt cur_value;
         cur_value.number.resize(0);
 
         for (int i = n - 1; i >= 0; --i) {
-			cur_value.number.insert(cur_value.number.begin(), number[i]);
-			cur_value.delete_zeros();
+            cur_value.number.insert(cur_value.number.begin(), number[i]);
+            cur_value.delete_zeros();
 
-			int div_left = 0, div_right = BASE;
+            int div_left = 0, div_right = BASE;
 
-			while (div_right - div_left > 1) {
-				int div_middle = (div_left + div_right) / 2;
+            while (div_right - div_left > 1) {
+                int div_middle = (div_left + div_right) / 2;
 
-				if (sec * div_middle <= cur_value) {
+                if (sec * div_middle <= cur_value) {
                     div_left = div_middle;
                 } else {
                     div_right = div_middle;
                 }
-			}
+            }
 
-			cur_value = cur_value - sec * div_left;
+            cur_value = cur_value - sec * div_left;
 
-			result.number.push_back(div_left);
-		}
+            result.number.push_back(div_left);
+        }
 
-		std::reverse(result.number.begin(), result.number.end());
-		result.delete_zeros();
+        std::reverse(result.number.begin(), result.number.end());
+        result.delete_zeros();
 
-		return result;
-	}
+        return result;
+    }
 
-	BigInt operator % (const BigInt &a) const
-	{
-		return *this - a * (*this / a);
-	}
+    BigInt operator % (const BigInt &a) const
+    {
+        return *this - a * (*this / a);
+    }
 
-	friend std::istream &operator >> (std::istream &in, BigInt &a);
-	friend std::ostream &operator << (std::ostream &out, const BigInt &a);
-	friend BigInt abs(const BigInt &a);
-	friend BigInt pow(const BigInt &a, unsigned n);
+    friend std::istream &operator >> (std::istream &in, BigInt &a);
+    friend std::ostream &operator << (std::ostream &out, const BigInt &a);
+    friend BigInt abs(const BigInt &a);
+    friend BigInt pow(const BigInt &a, unsigned n);
 };
 
 std::istream &operator >> (std::istream &in, BigInt &a)
 {
-	std::string tmp;
-	in >> tmp;
-	a = BigInt(tmp);
+    std::string tmp;
+    in >> tmp;
+    a = BigInt(tmp);
 
-	return in;
+    return in;
 }
 
 std::ostream &operator << (std::ostream &out, const BigInt &a)
@@ -574,32 +574,32 @@ std::ostream &operator << (std::ostream &out, const BigInt &a)
         return out;
     }
 
-	if (a.sign == BigInt::negative) {
+    if (a.sign == BigInt::negative) {
         out << "-";
     }
 
-	out << a.number.back();
+    out << a.number.back();
     for (int i = static_cast<int>(a.size()) - 2; i >= 0; --i) {
         out << std::setw(9) << std::setfill('0') << a.number[i];
     }
 
-	return out;
+    return out;
 }
 
 BigInt abs(const BigInt &a)
 {
-	BigInt result = a;
-	result.sign = BigInt::positive;
+    BigInt result = a;
+    result.sign = BigInt::positive;
 
-	return result;
+    return result;
 }
 
 BigInt pow(const BigInt &a, unsigned n)
 {
-	if (n == 0)
-		return 1;
-	
-	BigInt tmp = pow(a, n / 2);
+    if (n == 0)
+        return 1;
+    
+    BigInt tmp = pow(a, n / 2);
 
-	return tmp * tmp * (n % 2 ? a : 1);
+    return tmp * tmp * (n % 2 ? a : 1);
 }
