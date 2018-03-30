12dff0de8fff51ebe7fb142c40995b25a5e0dd87
Katrin <katya.avilova@gmail.com>
2018-03-30 20:25:13 +0300

    hw2.02

--- homework/AvilovaEM/02/prime.cpp
+++ homework/AvilovaEM/02/prime.cpp
@@ -4,7 +4,7 @@
 
 // Data -- массив
 // Size -- размер
- 
+
 int binary_search(const int* arr, int n, int k, bool bl)
 {
     int left = 0;
@@ -40,54 +40,50 @@ int binary_search(const int* arr, int n, int k, bool bl)
     }
     return -1; 
 }
-
-int is_prime(int n)
+bool is_prime(int n)
 {
     int i = 2;
     if(n == 1)
-        return 1;
-    if(n == 0)
-        return 1;    
+        return false;        
     while(1)
     {
+        if(i*i > n)
+            return true;
         if(n % i == 0)
-            return 0;
-        else
-            if(i*i >= n)
-                return 1;
+            return false;
         ++i;            
     }
 }
 
-int* array_of_primes(void)
+bool* array_of_primes(const int size, bool* arr_prime)
 {
-    int* arr_prime = new int [100000];
-    
-    for(int i = 0; i < 100000; ++i)
+    for(int i = 0; i < size; ++i)
         arr_prime[i] = is_prime(i);            
         
     return arr_prime;        
 }
 
-int count_of_primes(int* arr_prime, int left, int right)
+int count_of_primes(bool* arr_prime, int left, int right)
 {
     int count = 0;
     for(int i = left; i < right; ++i)
-        if(arr_prime[Data[i]] == 1)
+        if(arr_prime[Data[i]] == true)
             count++;
-    
+
     return count;
 }
 
 int main(int argc, char* argv[])
 {
     int num_left = 0;
+    const int size = 100000;
+    bool* arr_prime = new bool [size];
     int num_right = 0;
     int left = 0;
     int right = 0;
     int count = 0;
-    int sum = 0;
-    int* arr_prime = array_of_primes();
+
+    arr_prime = array_of_primes(size, arr_prime);
 
     if(argc%2 == 0 || argc < 3)
         return -1;
@@ -100,23 +96,19 @@ int main(int argc, char* argv[])
         
         if(num_left > num_right)
         {
-            std::cout << 0 << std::endl;
+            std::cout << 0;
             return 0;
         }
 
     
-        left = binary_search(Data, Size, num_left, 1);
+       left = binary_search(Data, Size, num_left, 1);
         right = binary_search(Data, Size, num_right, 0);
-        if(left == -1 || right == -1)
-            return 0;
             
         count = count_of_primes(arr_prime, left, right);
-        sum += count;
+        std::cout << count << std::endl; 
     }
 
     free(arr_prime);    
-    std::cout << sum << std::endl; 
-    
 
     return 0;
 }
