bd53e41e25ed996a0b14ef554c54657bc83606ee
Алексей <aj.kuznecov@physics.msu.ru>
2018-04-03 13:36:31 +0300

    hw4 update

--- homework/Kuznetsov/04/matrix.h
+++ homework/Kuznetsov/04/matrix.h
@@ -49,24 +49,25 @@ public:
 		return Proxy(matrix[i], columnCount);
 	}
 	
-	void operator*=(int number) {
+	Matrix& operator*=(int number) {
 		for (size_t i = 0; i < rowCount; i++) {
 			for (size_t j = 0; j < columnCount; j++) {
 				matrix[i][j] = matrix[i][j] * number;
 			}
 		}
+		return *this;
 	}
 	bool operator==(const Matrix& other) const{
 		if (other.rowCount != rowCount || other.columnCount != columnCount)
-			return 0;
+			return false;
 
 		for (size_t i = 0; i < rowCount; i++) {
 			for (size_t j = 0; j < columnCount; j++) {
 				if (matrix[i][j] != other.matrix[i][j])
-					return 0;
+					return false;
 			}
 		}
-		return 1;
+		return true;
 	}
 	bool operator!=(const Matrix& other) const{
 		return !(*this == other);
