abf25561b29a3d1daaa54402ed74f6a552b92806
vitek <viktor.bayanov1993@mial.ru>
2018-04-11 16:22:16 +0300

    exchenge function by class

--- homework/Bayanov/06/main.cpp
+++ homework/Bayanov/06/main.cpp
@@ -3,102 +3,112 @@
 #include <cstring>
 
 template<class T>
-T calc(char* str){
+class Calc{
+public:
+    T operator()(char* str);
 
-}
+};
 
-inline bool IsOperation(char f){
-    return f == '+' || f == '-' || f == '*' || f == '/';
-}
 
-inline bool IsDigit(char f){
-    return '0' <= f && f <= '9';
-}
 
 template<>
-int calc<int>(char* str){
+class Calc<int> {
     std::vector<int> operands;
     std::vector<char> operations;
 
-    size_t len = 0;
-    size_t walk = 0;
-    while(str[walk]){
-        if(str[walk] != ' '){
-            if(!IsOperation(str[walk]) && !IsDigit(str[walk])){
-                throw std::logic_error("");
-            }
-            str[len++] = str[walk];
-        }
-        ++walk;
+    inline bool IsOperation(char f){
+        return f == '+' || f == '-' || f == '*' || f == '/';
     }
-    size_t ind = 0;
-    bool odd = true;
-    while(ind < len){
-        if(ind < len) {
-            if (odd) {
-                bool pos = true;
-                if (str[ind] == '-' || str[ind] == '+') {
-                    pos = str[ind] == '+';
-                    ++ind;
-                }
-                if (ind >= len || !IsDigit(str[ind])) {
+
+    inline bool IsDigit(char f){
+        return '0' <= f && f <= '9';
+    }
+
+public:
+    int operator()(char *str) {
+        operands.clear();
+        operations.clear();
+
+        size_t len = 0;
+        size_t walk = 0;
+        while (str[walk]) {
+            if (str[walk] != ' ') {
+                if (!IsOperation(str[walk]) && !IsDigit(str[walk])) {
                     throw std::logic_error("");
                 }
-                long long value = 0;
-                while (ind < len && IsDigit(str[ind])) {
-                    value = value * 10 + (str[ind] - '0');
-                    if (value > (1ll << 31) - 1) {
+                str[len++] = str[walk];
+            }
+            ++walk;
+        }
+        size_t ind = 0;
+        bool odd = true;
+        while (ind < len) {
+            if (ind < len) {
+                if (odd) {
+                    bool pos = true;
+                    if (str[ind] == '-' || str[ind] == '+') {
+                        pos = str[ind] == '+';
+                        ++ind;
+                    }
+                    if (ind >= len || !IsDigit(str[ind])) {
                         throw std::logic_error("");
                     }
-                    ++ind;
-                }
-                if (!pos) {
-                    value *= -1;
-                }
-                operands.push_back(value);
-                if(operations.size() && (operations.back() == '*' ||
-                                      operations.back() == '/')){
-                    int a = operands.back();
-                    operands.pop_back();
-                    int b = operands.back();
-                    operands.pop_back();
-                    int res = b;
-                    if(operations.back() == '*'){
-                        res *= a;
-                    }else{
-                        if(!a){
+                    long long value = 0;
+                    while (ind < len && IsDigit(str[ind])) {
+                        value = value * 10 + (str[ind] - '0');
+                        if (value > (1ll << 31) - 1) {
                             throw std::logic_error("");
                         }
-                        res /= a;
+                        ++ind;
                     }
-                    operations.pop_back();
-                    operands.push_back(res);
-                }
-            } else {
-                if (!IsOperation(str[ind])) {
-                    throw std::logic_error("");
+                    if (!pos) {
+                        value *= -1;
+                    }
+                    operands.push_back(value);
+                    if (operations.size() && (operations.back() == '*' ||
+                                              operations.back() == '/')) {
+                        int a = operands.back();
+                        operands.pop_back();
+                        int b = operands.back();
+                        operands.pop_back();
+                        int res = b;
+                        if (operations.back() == '*') {
+                            res *= a;
+                        } else {
+                            if (!a) {
+                                throw std::logic_error("");
+                            }
+                            res /= a;
+                        }
+                        operations.pop_back();
+                        operands.push_back(res);
+                    }
                 } else {
-                    operations.push_back(str[ind]);
-                    ++ind;
+                    if (!IsOperation(str[ind])) {
+                        throw std::logic_error("");
+                    } else {
+                        operations.push_back(str[ind]);
+                        ++ind;
+                    }
                 }
             }
-        }
 
-        odd ^= true;
-    }
-    if(operands.size() != operations.size() + 1 || !operands.size()){
-        throw std::logic_error("");
-    }
-    int result = operands[0];
-    for(size_t it = 1; it < operands.size(); ++it){
-        if(operations[it - 1] == '+'){
-            result += operands[it];
-        }else{
-            result -= operands[it];
+            odd ^= true;
         }
+        if (operands.size() != operations.size() + 1 || !operands.size()) {
+            throw std::logic_error("");
+        }
+        int result = operands[0];
+        for (size_t it = 1; it < operands.size(); ++it) {
+            if (operations[it - 1] == '+') {
+                result += operands[it];
+            } else {
+                result -= operands[it];
+            }
+        }
+        return result;
     }
-    return result;
-}
+};
 
 int main(int argc, char* argv[]) {
     if(argc != 2){
@@ -106,8 +116,10 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    Calc<int> calc;
+
     try{
-        int value = calc<int>(argv[1]);
+        int value = calc(argv[1]);
         std::cout << value << "\n";
     }catch(std::logic_error){
         std::cout << "error";
