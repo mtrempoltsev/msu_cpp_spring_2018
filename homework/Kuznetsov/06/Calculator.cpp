e3860361d0bed7d20d75cac8edfd73c590b8ccc0
Алексей <aj.kuznecov@physics.msu.ru>
2018-04-19 11:50:50 +0300

    fix Calculator

--- homework/Kuznetsov/06/Calculator.cpp
+++ homework/Kuznetsov/06/Calculator.cpp
@@ -1,39 +1,69 @@
 #include <iostream>
 #include <string>
 #include <exception>
+#include <sstream>
+
 template<class T>class Calculator{
 private:
     T calculateStr(const std::string& str) const{
         for(size_t i = str.length(); i> 0; i--){
-                if(str[i] == '+')
+            if(str[i] == '+')
                 return multiple(str.substr(i+1)) +  calculateStr(str.substr(0,i));
-            else if(str[i] == '-' )
+            else if(str[i] == '-')
                 return calculateStr(str.substr(0,i)) - multiple(str.substr(i+1));
         }
         return multiple(str);
     }
     T multiple(const std::string& str) const{
         for(size_t i = str.length(); i> 0; i--){
-            if(str[i] == '*')
-                return multiple(str.substr(0,i))*std::stoi(str.substr(i+1));
-            else if(str[i] == '/')
-                return dividion(str.substr(0,i),std::stoi(str.substr(i+1)));
+            if(str[i] == '*') {
+                std::string numberStr = str.substr(i+1);
+                std::istringstream iss (numberStr);
+                T value;
+                iss >> value;
+                assertRightTransformation(value, numberStr);
+                return multiple(str.substr(0, i)) * value;
+            }
+            else if(str[i] == '/') {
+                std::string numberStr = str.substr(i+1);
+                std::istringstream iss (numberStr);
+                T value;
+                iss >> value;
+                assertRightTransformation(value, numberStr);
+                return dividion(str.substr(0, i), value);
+            }
         }
-        return std::stoi(str);
+        std::istringstream iss (str);
+        T value;
+        iss >> value;
+        assertRightTransformation(value, str);
+        return value;
     }
     T dividion(const std::string& delimoe, const T delitel) const{
         for(size_t i = delimoe.length(); i> 0; i--){
-            if(delimoe[i] == '*')
-                return multiple(delimoe.substr(0,i)) * std::stoi(delimoe.substr(i+1)) /delitel;
-            else if(delimoe[i] == '/')
-                return multiple(delimoe.substr(0,i)) / std::stoi(delimoe.substr(i+1)) /delitel;
+            if(delimoe[i] == '*') {
+                std::string numberStr = delimoe.substr(i+1);
+                std::istringstream iss (numberStr);
+                T value;
+                iss >> value;
+                assertRightTransformation(value, numberStr);
+                return multiple(delimoe.substr(0, i)) * value / delitel;
+            } else if(delimoe[i] == '/') {
+                std::string numberStr = delimoe.substr(i+1);
+                std::istringstream iss (numberStr);
+                T value;
+                iss >> value;
+                assertRightTransformation(value, numberStr);
+                return multiple(delimoe.substr(0, i)) / value / delitel;
+            }
         }
 
         return multiple(delimoe)/delitel;
     }
     std::string deleteAllSpaces(const std::string& str)const {
         std::string out = "";
-        for(int s: str)
+        *str.begin();
+        for(auto s: str)
             if(s != ' ' && s != '\t')
                 out += s;
         return out;
@@ -49,6 +79,11 @@ private:
                 out+=str[i];
         }
         return out;
+    }
+    void assertRightTransformation(T t, const std::string& str) const{
+        std::string s = std::to_string(t);
+        if(s != str)
+            throw std::runtime_error("FORMAT ERROR");
     }
     void checkDividedByZero(const std::string& str)const{
         for(int i =1; i< str.length(); i++){
