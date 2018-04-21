51b01e70146b23f7b00bcd6b302c17a23fd37f2f
$ ! L \\// A <fsatka@mail.ru>
2018-04-21 21:43:40 +0300

    ping pong

---
+++ homework/Doronin/09/09.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+volatile int i = 0;
+volatile bool flaq = true;
+std::mutex mutex;
+
+void thread1()
+{
+    while(flaq)
+    {
+        std::lock_guard <std::mutex> lock(mutex);
+        {
+            if (i % 2 == 0 && i != 1000000)
+            {
+                std::cout << "ping" << std::endl;
+                i++;
+            }
+            if(i == 1000000)
+            {
+                flaq = false;
+            }
+        }
+    }
+}
+
+void thread2()
+{
+    while(flaq)
+    {
+        std::lock_guard<std::mutex> lock(mutex);
+        {
+            if (i % 2 == 1 && i != 1000000)
+            {
+                std::cout << "pong" << std::endl;
+                i++;
+            }
+            if(i == 1000000)
+            {
+                flaq = false;
+            }
+        }
+    }
+}
+
+int main()
+{
+    std::thread t1(thread1);
+    std::thread t2(thread2);
+    t1.join();
+    t2.join();
+    return 0;
+}
