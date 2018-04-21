a8333a6bf2f9097e49d789b8cd3eaaa0ac1cf1c3
Alexander Chernyshev <alexander.v.chernyshev@mail.ru>
2018-04-21 23:18:03 +0300

    Homework 09 solved

---
+++ homework/Chernyshev/09/ping_pong_without_condition_variable.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+namespace Limits
+{
+    constexpr size_t MAX_ITERS = 1'000'000;
+}
+
+std::mutex mutex;
+volatile int cnt_iters = 0;
+
+void thread_function(int parity)
+{
+    for (;;) {
+        std::lock_guard<std::mutex> lg(mutex);
+        if (cnt_iters >= Limits::MAX_ITERS) {
+            break;
+        }
+
+        if ((cnt_iters & 1) == parity) {
+            std::cout << (parity == 0 ? "ping" : "pong") << std::endl;
+            ++cnt_iters;
+        }
+    }
+}
+
+int main()
+{
+    std::thread t0(thread_function, 0);
+    std::thread t1(thread_function, 1);
+
+    t0.join();
+    t1.join();
+}
---
+++ homework/Chernyshev/09/readme.md
@@ -0,0 +1,21 @@
+# Homework 09
+Write standard ping-pong programm
+
+## Building and running
+To build project:
+```
+make
+```
+
+To run project (you'll get output of 1'000'000 'ping' and 'pong' strings):
+```
+./ping_pong_without_condition_variable
+```
+
+## Cleaning directory
+```
+make clean
+```
+
+## Working time
+On my computer working time was about 9 seconds.
