d090dfa31ae2cd154bab60c2e8f609e794b24115
Gleb <gleb-skiba@mail.ru>
2018-04-25 15:32:41 +0300

    Remake

--- homework/Skiba/10/main.cpp
+++ homework/Skiba/10/main.cpp
@@ -1,27 +1,64 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <condition_variable>
 
+const int MAXN = 1000000;
 
 int a = 0;
 std::mutex mutex;
+std::mutex fp;
+std::mutex sp;
+std::condition_variable cv1;
+std::condition_variable cv2;
 
 void pr1() {
-    while (a < 1000000) {
-        if (!(a%2)) {
-            std::lock_guard<std::mutex> lock(mutex);
-            std::cout << "ping" << std::endl;
+    if (!a) {
+            std::lock_guard<std::mutex> g(mutex);
             a++;
+            std::cout << "ping" << std::endl;
+            cv2.notify_one();
+    }
+    while (true) {
+        std::unique_lock<std::mutex> lock(fp);
+        while (a%2 == 1) {
+            cv1.wait(lock);
+            if (a >= MAXN) {
+                std::lock_guard<std::mutex> g(mutex);
+                cv2.notify_one();
+                return;
+            }
+        }
+        {
+            std::lock_guard<std::mutex> g(mutex);
+            a++;
+            std::cout << "ping" << std::endl;
+            cv2.notify_one();
         }
     }
 }
 
 void pr2() {
-    while (a < 1000000) {
-        if (a%2) {
-            std::lock_guard<std::mutex> lock(mutex);
-            std::cout << "pong" << std::endl;
+    while (true) {
+        std::unique_lock<std::mutex> lock(sp);
+        while (a%2 == 0) {
+            cv2.wait(lock);
+            if (a >= MAXN) {
+                std::lock_guard<std::mutex> g(mutex);
+                cv1.notify_one();
+                return;
+            }
+        }
+        {
+            if (a >= MAXN) {
+                std::lock_guard<std::mutex> g(mutex);
+                cv1.notify_one();
+                return;
+            }
+            std::lock_guard<std::mutex> g(mutex);
             a++;
+            std::cout << "pong" << std::endl;
+            cv1.notify_one();
         }
     }
 }
