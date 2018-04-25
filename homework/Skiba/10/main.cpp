b32c6b653504e67066080e7ddcc0f4d6680f5dc8
Gleb <gleb-skiba@mail.ru>
2018-04-25 20:50:35 +0300

    Remake

--- homework/Skiba/10/main.cpp
+++ homework/Skiba/10/main.cpp
@@ -2,68 +2,70 @@
 #include <thread>
 #include <mutex>
 #include <condition_variable>
+#include <atomic>
 
 const int MAXN = 1000000;
 
-int a = 0;
-std::mutex mutex;
-std::mutex fp;
-std::mutex sp;
-std::condition_variable cv1;
-std::condition_variable cv2;
+std::atomic<int> a;
+std::mutex mutex1, mutex2;
+std::condition_variable cv;
 
 void pr1() {
     if (!a) {
-            std::lock_guard<std::mutex> g(mutex);
+            std::lock_guard<std::mutex> g(mutex1);
             a++;
             std::cout << "ping" << std::endl;
-            cv2.notify_one();
+            cv.notify_one();
     }
+
     while (true) {
-        std::unique_lock<std::mutex> lock(fp);
+        std::unique_lock<std::mutex> lock(mutex1);
         while (a%2 == 1) {
-            cv1.wait(lock);
+            cv.wait(lock);
             if (a >= MAXN) {
-                std::lock_guard<std::mutex> g(mutex);
-                cv2.notify_one();
+                std::lock_guard<std::mutex> g(mutex2);
+                cv.notify_all();
                 return;
             }
         }
+
         {
-            std::lock_guard<std::mutex> g(mutex);
+            std::lock_guard<std::mutex> g(mutex2);
             a++;
             std::cout << "ping" << std::endl;
-            cv2.notify_one();
+            cv.notify_all();
         }
     }
 }
 
 void pr2() {
     while (true) {
-        std::unique_lock<std::mutex> lock(sp);
+        std::unique_lock<std::mutex> lock(mutex1);
         while (a%2 == 0) {
-            cv2.wait(lock);
+            cv.wait(lock);
             if (a >= MAXN) {
-                std::lock_guard<std::mutex> g(mutex);
-                cv1.notify_one();
+                std::lock_guard<std::mutex> g(mutex2);
+                cv.notify_all();
                 return;
             }
         }
+
         {
             if (a >= MAXN) {
-                std::lock_guard<std::mutex> g(mutex);
-                cv1.notify_one();
+                std::lock_guard<std::mutex> g(mutex2);
+                cv.notify_all();
                 return;
             }
-            std::lock_guard<std::mutex> g(mutex);
+            std::lock_guard<std::mutex> g(mutex2);
             a++;
             std::cout << "pong" << std::endl;
-            cv1.notify_one();
+            cv.notify_all();
         }
     }
 }
 
 int main(int argc, char *argv[]) {
+    a.store(0);
     std::thread t1(pr1);
     std::thread t2(pr2);
     t1.join();
