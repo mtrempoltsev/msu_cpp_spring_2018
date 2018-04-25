35043c6e9e87e2ee2deca625fcd8eb8af5ed5e3c
Mergen <imeevma@gmail.com>
2018-04-25 19:01:34 +0300

    HW08 map to unordered_map and set to map

--- homework/Imeev/08/hw08.cpp
+++ homework/Imeev/08/hw08.cpp
@@ -1,14 +1,13 @@
+#include <unordered_map>
 #include <algorithm>
 #include <stdexcept>
 #include <iostream>
 #include <fstream>
-#include <set>
 #include <map>
 
 constexpr size_t ARGNUM = 2;
 constexpr size_t TOP_N = 10;
 
-class wrong_argument_number: public std::exception {};
 class couldnt_open_file: public std::exception {};
 
 void word_counter(const std::string &fname);
@@ -17,7 +16,8 @@ int main(int argc, char **argv)
 {
     try {
         if(argc != ARGNUM) {
-            throw wrong_argument_number();
+            std::cerr << "error" << std::endl;
+            return 1;
         }
         word_counter(argv[1]);
     } catch(...) {
@@ -33,14 +33,14 @@ void word_counter(const std::string &fname)
     if (!fin) {
         throw couldnt_open_file();
     }
-    std::map<std::string, int> counter;
+    std::unordered_map<std::string, int> counter;
     while (fin.good()) {
         std::string word;
         fin >> word;
         counter[word]++;
     }
 
-    std::set<std::pair<int, std::string>> res;
+    std::map<int, std::string> res;
     for(auto &elem : counter) {
         res.emplace(elem.second, elem.first);
     }
