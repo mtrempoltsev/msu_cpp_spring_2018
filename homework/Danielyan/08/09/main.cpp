768033bb3f1d3d6576719296c216f95d14e59bcc
Suren Danielyan <knoxja@yandex.ru>
2018-04-27 23:37:12 +0300

    Initial commit

---
+++ homework/Danielyan/08/09/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <thread>
+#include <atomic>
+
+
+int main(int argc, char **argv) {
+    std::atomic<bool> ping_time{false};
+    std::thread ping([&ping_time] () {
+        for (size_t i = 0; i < 1000000; ++i) {
+            while (!ping_time) {
+            }
+            std::cout << "ping\n";
+            ping_time.store(false);
+        }
+    });
+    std::thread pong([&ping_time]() {
+        for (size_t i = 0; i < 1000000; ++i) {
+            while (ping_time) {
+            }
+            std::cout << "pong\n";
+            ping_time.store(true);
+        }
+    });
+
+    ping.join();
+    pong.join();
+
+    return 0;
+}
+
