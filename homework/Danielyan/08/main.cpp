02628e01d41f27f2683ab20e098d463a738eb5af
Suren Danielyan <knoxja@yandex.ru>
2018-04-27 22:55:58 +0300

    Fixed wrong comparsion fucntion, improved efficiency

--- homework/Danielyan/08/main.cpp
+++ homework/Danielyan/08/main.cpp
@@ -4,22 +4,23 @@
 #include <map>
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 auto CountWordOccurrences(std::ifstream &in) {
-    std::map<std::string, uint64_t> dict;
+    std::unordered_map<std::string, uint64_t> dict;
     std::string cur_word;
     while (in >> cur_word)
         ++dict[cur_word];
     return dict;
 };
 
-auto SortKeysByValues(const std::map<std::string, uint64_t> &map) {
+auto SortKeysByValues(const std::unordered_map<std::string, uint64_t> &map) {
     std::vector<std::pair<uint64_t, std::string>> result;
     result.reserve(map.size());
     for (auto &x : map)
         result.emplace_back(x.second, x.first);
     std::sort(result.begin(), result.end(), [] (const auto& x, const auto& y) {
-        return x.first < y.first;
+        return x.first > y.first;
     });
     return result;
 }
@@ -33,10 +34,11 @@ int main(int argc, char **argv) {
     std::ifstream input(argv[1]);
     if (!input.is_open()) {
         std::cerr << "Could not open the file" << std::endl;
+        return 1;
     }
 
     auto result = SortKeysByValues(CountWordOccurrences(input));
-    for (int i = 0; i < result.size() && i < 10; ++i) {
+    for (int i = 0; i < std::min<size_t>(result.size(), 10); ++i) {
         std::cout << result[i].first << " " << result[i].second << "\n";
     }
 
