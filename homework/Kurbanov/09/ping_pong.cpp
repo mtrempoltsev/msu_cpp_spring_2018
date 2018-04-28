6d1c04700508e71887c85f6633f7ec4a93fbba6a
Demontego <36605450+Demontego@users.noreply.github.com>
2018-04-29 01:34:14 +0300

    Update ping_pong.cpp

--- homework/Kurbanov/09/ping_pong.cpp
+++ homework/Kurbanov/09/ping_pong.cpp
@@ -1,33 +1,37 @@
 #include <iostream>
 #include <thread>
+#include<atomic>
 #include <mutex>
-#include <condition_variable>
-
+const std::uint32_t MAX = 1000000;
+std::atomic<int> i(0);
 std::mutex mutex;
 std::condition_variable ready;
 
 void ping()
 {
 	std::unique_lock<std::mutex> lock(mutex);
-	for (int i = 0; i < 10; i++)
+	while (i < MAX)
 	{
+		while (i % 2 == 1)
+			ready.wait(lock);
 		std::cout << "ping" << std::endl;
+		i++;
 		ready.notify_one();
-		ready.wait(lock);
 	}
-	ready.notify_one();
 }
+
 
 void pong()
 {
 	std::unique_lock<std::mutex> lock(mutex);
-	for (int i = 0; i < 10; i++)
+	while (i < MAX+1)//чтобы красиво на pong заканчивалось
 	{
+		while (i % 2 == 0)
+			ready.wait(lock);
 		std::cout << "pong" << std::endl;
+		i++;
 		ready.notify_one();
-		ready.wait(lock);
 	}
-	ready.notify_one();
 }
 
 int main() 
