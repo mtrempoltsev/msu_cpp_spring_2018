228a257048a4d832dd92ba4518e66a1f1cc45281
ImeevMA <imeevma@gmail.com>
2018-04-28 14:36:50 +0300

    HW09 back to safety

--- homework/Imeev/09/hw09.cpp
+++ homework/Imeev/09/hw09.cpp
@@ -1,13 +1,12 @@
 #include <condition_variable>
+#include <iostream>
 #include <atomic>
-#include <cstdio>
 #include <thread>
 #include <mutex>
 
 std::mutex m;
 std::condition_variable cv;
 
-constexpr size_t str_size = 5;
 constexpr int MAX_ITER = 500000;
 std::atomic<bool> is_pong_time(false);
 
@@ -29,20 +28,12 @@ int main()
 
 void printer(bool value)
 {
-    char str_ping[] = "ping\n";
-    char str_pong[] = "pong\n";
-    char *str;
-    if(value) {
-        str = str_pong;
-    } else {
-        str = str_ping;
-    }
+    const char* str = value ? "pong\n" : "ping\n";
     for(int i = 0; i < MAX_ITER; ++i) {
         std::unique_lock<std::mutex> lk(m);
         cv.wait(lk, [value]{return is_pong_time == value;});
-        fwrite(str, sizeof(char), str_size, stdout);
+        std::cout << str << "\n";
         is_pong_time = !value;
-        lk.unlock();
         cv.notify_one();
     }
 }
