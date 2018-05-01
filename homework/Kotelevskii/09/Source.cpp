a91c109b0642113f991353ccbab1baa07bb435d4
Nikita Kotelevskii <36699718+nkotelevskii@users.noreply.github.com>
2018-05-01 12:12:03 +0300

    Update Source.cpp

--- homework/Kotelevskii/09/Source.cpp
+++ homework/Kotelevskii/09/Source.cpp
@@ -1,38 +1,25 @@
 #include<thread>
 #include<iostream>
-#include <atomic>
-std::thread::id* first = nullptr;
-std::atomic<bool> checker(true);
-void thread()
+#include <mutex>
+#include <condition_variable>
+std::mutex m;
+std::condition_variable cond_var;
+bool notified = true;
+void thread(bool first)
 {
-	std::thread::id this_id = std::this_thread::get_id();
-	if (first == nullptr)
-	{
-		first = new std::thread::id();
-		*first = this_id;
-	}
 	for (int i = 0; i < 500000; i++)
 	{
-		if (this_id == *first)
-		{
-			while (!checker);
-			std::cout << "ping" << std::endl;
-			checker = false;
-		}
-		else
-		{
-			while (checker);
-			std::cout << "pong" << std::endl;
-			checker = true;
-		}
+		std::unique_lock<std::mutex> lock(m);
+		cond_var.wait(lock, [first]() {return first ? notified == true : notified != true; });
+		first ? std::cout << "ping" << std::endl : std::cout << "pong" << std::endl;
+		notified = first ? false : true;
+		cond_var.notify_one();
 	}
-	delete first;
-	first = nullptr;
 }
 int main()
 {
-	std::thread t1(thread);
-	std::thread t2(thread);
+	std::thread t1(thread, true);
+	std::thread t2(thread, false);
 	t1.join();
 	t2.join();
 }
