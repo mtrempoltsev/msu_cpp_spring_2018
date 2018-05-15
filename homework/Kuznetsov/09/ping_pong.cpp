30cd92e849229cbc5a6041f3afc33bbdd42739db
Алексей <aj.kuznecov@physics.msu.ru>
2018-05-15 17:57:29 +0300

    update ping pong

--- homework/Kuznetsov/09/ping_pong.cpp
+++ homework/Kuznetsov/09/ping_pong.cpp
@@ -6,40 +6,34 @@
 const size_t ITERS = 1000000;
 std::mutex m;
 std::condition_variable conditionVariable;
-volatile int count = 0;
-volatile bool ping_start_flag = false;
+int count = 0;
 
 
 void ping(){
     std::unique_lock<std::mutex> lock(m);
-    ping_start_flag = true;
     while(count <= ITERS) {
 
-        //while(count % 2 == 1)
-        conditionVariable.notify_one();
-        conditionVariable.wait(lock);
+        while (count % 2 == 1)
+            conditionVariable.wait(lock);
 
         std::cout << "ping" << std::endl;
         ++count;
+        conditionVariable.notify_one();
     }
-    conditionVariable.notify_one();
 }
 void pong(){
     std::unique_lock<std::mutex> lock(m);
 
-    while(!ping_start_flag)
-        conditionVariable.wait(lock);
-
     while(count <= ITERS) {
 
-        //while (count % 2 == 0)
-        conditionVariable.notify_one();
-        conditionVariable.wait(lock);
+        while (count % 2 == 0)
+            conditionVariable.wait(lock);
+
         std::cout << "pong" << std::endl;
         ++count;
+        conditionVariable.notify_one();
     }
-    conditionVariable.notify_one();
-} 
+}
 
 int main() {
     std::thread thread(ping);
