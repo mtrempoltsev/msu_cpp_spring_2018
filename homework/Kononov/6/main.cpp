6a4c63cdaf8afedd369c7e3f45b34c617c8c4890
Sergey Kononov <lalkakonus@gmail.com>
2018-06-04 02:03:24 +0300

    Add files via upload

--- homework/Kononov/6/main.cpp
+++ homework/Kononov/6/main.cpp
@@ -1,1 +1,22 @@
- 
+// Kononov Sergey BD-11
+
+#include "calc.h"
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+	if (argc != 2) {
+		std::cerr << "error\n";
+		return 1;
+	}
+	
+	Calculator <int> c;
+	try {
+		std::cout << c.calculate(argv[1]) << std::endl;
+	}
+	catch(CalcError& error) {
+		std::cerr << "Error\n";
+		return 1;
+	}
+	
+	return 0;
+}
