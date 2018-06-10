27c7f2ccbee286e7b791d9e89a429986d68f5caa
Jusill <Jusill98@gmail.com>
2018-06-10 07:58:14 +0300

    Add files via upload

--- homework/Konstantinovsky/06/main.cpp
+++ homework/Konstantinovsky/06/main.cpp
@@ -3,14 +3,14 @@
 #include "CalcAlg.h"
 
 int main(int argc, char *argv[]) {
-	/*
+	
 	try {
 		if (argc != 2) {
 			throw std::runtime_error("error");
 		}
 
 		Calculator<int> calc;
-		std::cout << calc.compute(argv[1]) << std::endl;
+		std::cout << calc.calculate(argv[1]) << std::endl;
 	}
 	catch (std::runtime_error err) {
 		std::cout << err.what() << std::endl;
@@ -22,9 +22,7 @@ int main(int argc, char *argv[]) {
 
 		return -2;
 	}
-	*/
-	std::string str = "sfg fdgfd fdsg";
-	str.erase(std::remove_if(str.begin(), str.end(), isspace), str.end());
-	std::cout << str;
+	
+
 	return 0;
 }
