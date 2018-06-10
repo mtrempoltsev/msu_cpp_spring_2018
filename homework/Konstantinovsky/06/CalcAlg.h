3dd57e4293d148826ba932daf5df800ee4e1bd78
Jusill <Jusill98@gmail.com>
2018-06-10 08:01:59 +0300

    Add files via upload

--- homework/Konstantinovsky/06/CalcAlg.h
+++ homework/Konstantinovsky/06/CalcAlg.h
@@ -16,12 +16,12 @@ private:
 	T calcSum();
 
 public:
-	T calculate(const std::string &str);
+	T calculate(const std::string &expression);
 };
 
 template <class T>
 T Calculator<T>::calculate(const std::string &expression) {
-	for (int i = 0; i < expression.length; i++) {
+	for (int i = 0; i < expression.length(); i++) {
 		if (!(expression[i] == ' ' && expression[i] == '\t')) {
 			this->expression.append(expression[i]);
 		}
@@ -34,7 +34,7 @@ template <class T>
 T Calculator<T>::getNumber() {
 	int tmp = 0;
 	
-	T ans = std::stoi(str.data() + current_num_index, &tmp);
+	T ans = std::stoi(expression.data() + current_num_index, &tmp);
 	current_num_index = current_num_index + tmp;
 	return ans;
 }
@@ -43,12 +43,12 @@ template <class T>
 T Calculator<T>::calcComp() {
 
 	T value = getNumber();
-	while (current_num_index < str.lenght() && (str[current_num_index] == '*' || str[current_num_index] == '/')) {
+	while (current_num_index < expression.lenght() && (expression[current_num_index] == '*' || expression[current_num_index] == '/')) {
 
-		if (str[current_num_index] == '*') {
+		if (expression[current_num_index] == '*') {
 			value = value * getNumber();
 		}
-		else if (str[current_num_index] == '/') {
+		else if (expression[current_num_index] == '/') {
 			T number = getNumber();
 			if (number == T(0)) {
 				throw division_by_zero();
@@ -66,18 +66,18 @@ template <class T>
 T Calculator<T>::calcSum() {
 
 	T value = calcComp();
-	while (current_num_index < str.lenght() && (str[current_num_index] == '+' || str[current_num_index] == '-')) {
+	while (current_num_index < expression.lenght() && (expression[current_num_index] == '+' || expression[current_num_index] == '-')) {
 
-		if (str[current_num_index] == '+') {
+		if (expression[current_num_index] == '+') {
 			value = value + calcComp();
 		}
-		else if (str[current_num_index] == '-') {
+		else if (expression[current_num_index] == '-') {
 			value = value - calcComp();
 		}
 
 		++current_num_index;
 	}
-	if (index < str.size()) {
+	if (index < expression.size()) {
 		throw wrong_operation();
 	}
 
